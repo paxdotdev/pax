124361091462a0a92f55079b0e64833121552b86
Zack Brown <zack@inclination.co>
2021-09-29 13:57:09 -0700

    wip: (broken build) progress with data structures, rust

--- carbon/src/lib.rs
+++ carbon/src/lib.rs
@@ -1,18 +1,12 @@
-pub use piet_web::WebRenderContext;
-pub use piet::{Error, Color};
+use kurbo::{Affine, BezPath, Point, Vec2};
+pub use piet::{Color, Error};
 use piet::RenderContext;
-
-use kurbo::{Rect, BezPath, Point, Size, Affine, Vec2};
+pub use piet_web::WebRenderContext;
 
 pub struct CarbonEngine {
     // tick_and_render: fn(&mut Context) -> Result<(), Error>
-    frames_elapsed: u32
-}
-
-pub struct Transform {
-    translate: (f64, f64),
-    scale: (f64, f64),
-    rotate: f64 //z only for 2D
+    frames_elapsed: u32,
+    scene_graph: SceneGraph,
 }
 
 pub struct Stroke {
@@ -24,12 +18,33 @@ pub struct Fill {
     solid: Color,
 }
 
-pub struct Node {
-    children: Vec<Rectangle>
+pub struct Group {
+    children: Vec<Box<dyn RenderNode>>
+}
+
+trait RenderNode {
+    fn get_children(&self) -> &Vec<Box<dyn RenderNode>>;
+}
+
+impl RenderNode for Group {
+    fn get_children(&self) -> &Vec<Box<dyn RenderNode>> {
+        &self.children
+    }
+}
+
+static NO_CHILDREN : Vec<Box<dyn RenderNode>> = Vec::new();
+
+impl RenderNode for Rectangle {
+    fn get_children(&self) -> &Vec<Box<dyn RenderNode>> {
+
+
+        return &NO_CHILDREN;
+
+    }
 }
 
 pub struct SceneGraph {
-    root: Node
+    root: Group
 }
 
 // base class for scene graph entities
@@ -38,13 +53,13 @@ pub struct Rectangle {
     height: f64,
     stroke: Stroke,
     fill: Fill,
-    transform: Transform
+    transform: Affine,
 }
 
 //TODO:  decorate with renderable trait
 //TODO:  organize alongside other nodes in fs, modules
 impl Rectangle {
-    fn new(width: f64, height: f64, stroke: Stroke, fill: Fill, transform: Transform) -> Self {
+    fn new(width: f64, height: f64, stroke: Stroke, fill: Fill, transform: Affine) -> Self {
         Rectangle {
             width,
             height,
@@ -65,84 +80,109 @@ pub fn get_engine() -> CarbonEngine {
 
 impl CarbonEngine {
     fn new() -> Self {
-        let scene_graph = SceneGraph {
-            root: Node {
-                children: vec![
-                    Rectangle {
-                        width: 200.0,
-                        height: 200.0,
-                        stroke: Stroke {
-                            width: 1.0,
-                            solid: Color::rgb8(255, 0, 0),
-                        },
-                        fill: Fill {
-                          solid: Color::rgb8(0,255,0),
-                        },
-                        transform: Transform {
-                            scale: (1.0, 1.0),
-                            translate: (0., 0.),
-                            rotate: 0.,
-                        }
-                    },
-                    Rectangle {
-                        width: 100.0,
-                        height: 100.0,
-                        stroke: Stroke {
-                            width: 1.0,
-                            solid: Color::rgb8(255, 0, 255),
-                        },
-                        fill: Fill {
-                            solid: Color::rgb8(255,255,0),
-                        },
-                        transform: Transform {
-                            scale: (1.0, 1.0),
-                            translate: (250., 250.),
-                            rotate: 0.,
-                        }
-                    }
-                ]
-            }};
         CarbonEngine {
-            frames_elapsed: 0
+            frames_elapsed: 0,
+            scene_graph: SceneGraph {
+                root: Group {
+                    children: vec![
+                        Box::new(Rectangle {
+                            width: 200.0,
+                            height: 200.0,
+                            stroke: Stroke {
+                                width: 1.0,
+                                solid: Color::rgb8(255, 0, 0),
+                            },
+                            fill: Fill {
+                                solid: Color::rgb8(0, 255, 0),
+                            },
+                            transform: Affine::default(),
+                        }),
+                        Box::new(Rectangle {
+                            width: 100.0,
+                            height: 100.0,
+                            stroke: Stroke {
+                                width: 1.0,
+                                solid: Color::rgb8(255, 0, 255),
+                            },
+                            fill: Fill {
+                                solid: Color::rgb8(255, 255, 0),
+                            },
+                            transform: Affine::default(),
+                        })
+                    ]
+                }
+            },
         }
     }
 
-    fn render_scene_graph() -> Result<(), Error> {
+
+
+    fn render_scene_graph(&self) -> Result<(), Error> {
         // hello world scene graph
         //           (root)
         //           /    \
         //       (rect)  (rect)
 
+        // 1. find lowest node (last child of last node), accumulating transform along the way
+        // 2. start rendering, from lowest node on-up
+
+
+
+        self.recurse_render_scene_graph(&self.scene_graph.root);
+        Ok(())
+    }
+
+    fn recurse_render_scene_graph(&self, drawable: &impl RenderNode) -> Result<(), Error> {
+        // Recurse:
+        //  - iterate backwards over children (lowest first); recurse until there are no more descendants.  track transform matrix along the way.
+        //  - we now have the back-most leaf node.  Render it.  Return.
+        //  - we're now at the second back-most leaf node.  Render it.  Return ...
+        //  - done
+
 
         Ok(())
     }
 
-    pub fn tick_and_render (&mut self, rc: &mut WebRenderContext) -> Result<(), Error> {
 
+    pub fn tick_and_render(&mut self, rc: &mut WebRenderContext) -> Result<(), Error> {
+        rc.clear(Color::rgb8(255, 255, 0));
+
+        self.render_scene_graph();
+
+        // traverse_scene_graph
+        //  - accumulate transforms
+        //  - draw drawables, * transforms
+        //  - support text
+        //  - support images
+        //  - support table, parameterized children (e.g. cell content)
+
+        Ok(())
+    }
+
+
+    pub fn tick_and_render_disco_taps(&mut self, rc: &mut WebRenderContext) -> Result<(), Error> {
         let hue = (((self.frames_elapsed + 1) as f64 * 2.0) as i64 % 360) as f64;
         let current_color = Color::hlc(hue, 75.0, 127.0);
         rc.clear(current_color);
 
         for x in 0..20 {
             for y in 0..12 {
-
-                let bp_width : f64 = 100.;
-                let bp_height : f64 = 100.;
+                let bp_width: f64 = 100.;
+                let bp_height: f64 = 100.;
                 let mut bez_path = BezPath::new();
-                bez_path.move_to(Point::new(-bp_width/2., -bp_height/2.));
-                bez_path.line_to(Point::new(bp_width/2., -bp_height/2.));
-                bez_path.line_to(Point::new(bp_width/2., bp_height/2.));
-                bez_path.line_to(Point::new(-bp_width/2., bp_height/2.));
-                bez_path.line_to(Point::new(-bp_width/2., -bp_height/2.));
+                bez_path.move_to(Point::new(-bp_width / 2., -bp_height / 2.));
+                bez_path.line_to(Point::new(bp_width / 2., -bp_height / 2.));
+                bez_path.line_to(Point::new(bp_width / 2., bp_height / 2.));
+                bez_path.line_to(Point::new(-bp_width / 2., bp_height / 2.));
+                bez_path.line_to(Point::new(-bp_width / 2., -bp_height / 2.));
                 bez_path.close_path();
 
-                let theta = self.frames_elapsed as f64 * (0.01 + (x as f64 + y as f64 + 10.) / 64.) / 10.;
+                let theta = self.frames_elapsed as f64 * (0.04 + (x as f64 + y as f64 + 10.) / 64.) / 10.;
                 let transform =
                     Affine::translate(Vec2::new(x as f64 * bp_width, y as f64 * bp_height)) *
                     Affine::rotate(theta) *
-                    Affine::scale(theta.sin() * 1.1)
+                    Affine::scale(theta.sin() * 1.2)
                 ;
-
 
                 let transformed_bez_path = transform * bez_path;
 
@@ -151,9 +191,6 @@ impl CarbonEngine {
                 rc.fill(transformed_bez_path, &phased_color);
             }
         }
-
-
-
 
         self.frames_elapsed = self.frames_elapsed + 1;
         Ok(())
