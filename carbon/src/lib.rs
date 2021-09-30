db4f1258f6a5790c295c0bde68b8a56b5cf38db2
Zack Brown <zack@inclination.co>
2021-09-30 05:45:19 -0700

    feat: progress; non-polymorphic scene graph + compilation

--- carbon/src/lib.rs
+++ carbon/src/lib.rs
@@ -5,78 +5,101 @@ pub use piet::{Color, Error};
 use piet::RenderContext;
 pub use piet_web::WebRenderContext;
 
-pub struct Stroke {
-    width: f64,
-    solid: Color,
-}
+//
+// pub struct Stroke {
+//     width: f64,
+//     solid: Color,
+// }
+//
+// pub struct Fill {
+//     solid: Color,
+// }
+//
+// pub struct Group {
+//     transform: Affine,
+//     children: Vec<Box<dyn RenderNode>>,
+// }
 
-pub struct Fill {
-    solid: Color,
-}
-
-pub struct Group {
-    transform: Affine,
-    children: Vec<Box<dyn RenderNode>>,
-}
 
 
-pub struct SceneGraph {
-    root: Box<Group>
-}
+
+
+//Simplify:
+// - Traverse a non-polymorphic tree (everything a Node)
+// - Then make polymorphic
 
 // base class for scene graph entities
-pub struct Rectangle {
-    width: f64,
-    height: f64,
-    stroke: Stroke,
-    fill: Fill,
+// pub struct Rectangle {
+//     width: f64,
+//     height: f64,
+//     stroke: Stroke,
+//     fill: Fill,
+//     transform: Affine,
+// }
+
+// trait RenderNode
+// {
+//     fn get_children(self) -> Vec<Box<dyn RenderNode>>;
+//     fn get_transform(self) -> Affine;
+// }
+//
+// impl RenderNode for Group
+// {
+//     fn get_children(self) -> Vec<Box<dyn RenderNode>> {
+//         self.children
+//     }
+//     fn get_transform(self) -> Affine {
+//         self.transform
+//     }
+// }
+//
+// impl RenderNode for Rectangle
+// {
+//     fn get_children(self) -> Vec<Box<dyn RenderNode>> {
+//         Vec::new()
+//     }
+//     fn get_transform(self) -> Affine {
+//         self.transform
+//     }
+// }
+//
+// //TODO:  organize alongside other nodes in fs, modules
+// impl Rectangle {
+//     fn new(width: f64, height: f64, stroke: Stroke, fill: Fill, transform: Affine) -> Self {
+//         Rectangle {
+//             width,
+//             height,
+//             stroke,
+//             fill,
+//             transform, //TODO:  this should probably be SugaryTransform
+//         }
+//     }
+//     fn render(ctx: WebRenderContext) {
+//     }
+// }
+
+
+struct Node {
+    children: Vec<Node>,
     transform: Affine,
 }
 
-trait RenderNode
-{
-    fn get_children(self) -> Vec<Box<dyn RenderNode>>;
-    fn get_transform(self) -> Affine;
-}
-
-impl RenderNode for Group
-{
-    fn get_children(self) -> Vec<Box<dyn RenderNode>> {
-        self.children
+impl Node {
+    fn get_children(&self) -> Option<&Vec<Node>> {
+        Some(&self.children)
     }
-    fn get_transform(self) -> Affine {
-        self.transform
+    fn get_transform(&self) -> &Affine {
+        &self.transform
     }
 }
 
-impl RenderNode for Rectangle
-{
-    fn get_children(self) -> Vec<Box<dyn RenderNode>> {
-        Vec::new()
-    }
-    fn get_transform(self) -> Affine {
-        self.transform
-    }
-}
-
-//TODO:  organize alongside other nodes in fs, modules
-impl Rectangle {
-    fn new(width: f64, height: f64, stroke: Stroke, fill: Fill, transform: Affine) -> Self {
-        Rectangle {
-            width,
-            height,
-            stroke,
-            fill,
-            transform, //TODO:  this should probably be SugaryTransform
-        }
-    }
-    fn render(ctx: WebRenderContext) {
-    }
-}
-
-// Public method for consumption by engine chassis
+// Public method for consumption by engine chassis, e.g. WebChassis
 pub fn get_engine() -> CarbonEngine {
     return CarbonEngine::new();
+}
+
+pub struct SceneGraph {
+    root: Node
 }
 
 pub struct CarbonEngine {
@@ -91,28 +114,20 @@ impl CarbonEngine {
         CarbonEngine {
             frames_elapsed: 0,
             scene_graph: SceneGraph {
-                root: Box::new(Group {
+                root: Node {
                     transform: Affine::rotate(1.5),
                     children: vec![
-                        Box::new(Rectangle {
-                            width: 100.0,
-                            height: 100.0,
-                            stroke: Stroke {
-                                width: 1.0,
-                                solid: Color::rgb8(255, 0, 255),
-                            },
-                            fill: Fill {
-                                solid: Color::rgb8(255, 255, 0),
-                            },
+                        Node {
                             transform: Affine::translate(Vec2 { x: 300.0, y: 300.0 }) * Affine::rotate(1.2),
-                        }),
+                            children: Vec::new(),
+                        },
                     ],
-                })
+                },
             },
         }
     }
 
-    fn render_scene_graph(&mut self) -> Result<(), Error> {
+    fn render_scene_graph(&self) -> Result<(), Error> {
         // hello world scene graph
         //           (root)
         //           /    \
@@ -120,37 +135,43 @@ impl CarbonEngine {
 
         // 1. find lowest node (last child of last node), accumulating transform along the way
         // 2. start rendering, from lowest node on-up
-        self.recurse_render_scene_graph(self.scene_graph.root, &Affine::default());
+        self.recurse_render_scene_graph(&self.scene_graph.root, &Affine::default());
         Ok(())
     }
 
-    fn recurse_render_scene_graph<T: RenderNode>(&self, node: Box<T>, accumulated_transform: &Affine) -> Result<(), Error> {
+    fn recurse_render_scene_graph(&self, node: &Node, accumulated_transform: &Affine) -> Result<(), Error> {
         // Recurse:
         //  - iterate backwards over children (lowest first); recurse until there are no more descendants.  track transform matrix along the way.
         //  - we now have the back-most leaf node.  Render it.  Return.
         //  - we're now at the second back-most leaf node.  Render it.  Return ...
         //  - done
 
-        let new_accumulated_transform = *accumulated_transform * node.get_transform();
+        let new_accumulated_transform = *accumulated_transform * *node.get_transform();
 
-        let children = node.as_ref().get_children();
 
-        if children.len() == 0 {
-            //this is a leaf node.  render it.
 
-        } else {
-            //keep recursing
-            for i in (0..children.len() - 1).rev() {
-                //note that we're iterating starting from the last child
-                let child = children.get(i);
-                match child {
-                    None => { panic!() },
-                    Some(child) => {
-                       &self.recurse_render_scene_graph(child, &new_accumulated_transform);
+        match node.get_children() {
+            Some(children) => {
+                //keep recursing
+                for i in (0..children.len() - 1).rev() {
+                    //note that we're iterating starting from the last child
+                    let child = children.get(i); //TODO: ?-syntax
+                    match child {
+                        None => { panic!() },
+                        Some(child) => {
+                            &self.recurse_render_scene_graph(child, &new_accumulated_transform);
+                            return Ok(());
+                        }
                     }
                 }
+            },
+            None => {
+                //this is a leaf node.  render it.
+                //TODO:  some nodes (e.g. layouts) can have children AND require rendering.
+                //       also: some nodes are virtual, e.g. $repeat
             }
         }
+
 
         Ok(())
     }
