5df6991d410532959aa27f8619eff9d0d188afc7
Zack Brown <zack@inclination.co>
2021-10-17 13:33:22 -0700

    wip: start penciling in a (probably-incorrect) grammar for templates

--- carbon-template-compiler/src/main.rs
+++ carbon-template-compiler/src/main.rs
@@ -4,14 +4,9 @@
 TODO:
     [ ] CLI + args parsing
         [ ] Pass path to HTML file or multiple files, or to a .json config
-    [ ] DOM parser
-        [ ] Headless browser load HTML file
-    [ ] Double-brace (`{{}}`) templating
-        [ ] Parsing mechanism for `{{}}`, meeting half-way on syntax if needed
-        [ ] Mechanism for mapping rust fragments to PropertyValues
-            [ ] Sanity-check home-rolled expression syntax, PEG
-    [ ] wasm compiler-chassis
-        [ ] Embed alongside loaded HTML, grab DOM structure and attributes
+    [ ] Template parser
+    [ ] Properties parser
+    [ ] Expression parser
 
 */
 
@@ -20,18 +15,24 @@ TODO:
 /*
 COMPILATION STAGES
 
-0. Process double-braces
-    - parse file for {{}} pairs, swapping content with a known string attribute,
-      or otherwise referencable property at runtime in the hydrated DOM
-    - either: compile the expression language via PEG to rust and then compile onto target
-          or: copy-paste the provided code snippets into a generated rust wrapper file, compile
-       * Keep in mind * whatever approach is viable for "code-behind" (`<script type="application/x-rust">`)
-1. Load compiled HTML into headless browser, along with wasm compiler-chassis
-    - Pull .rs code out of `<script>` declarations, plop into templates, compile with rustc/cargo
-    - Use web-sys to parse the hydrated DOM and map into carbon """bytecode"""
-
+0. Process template
+    - build render tree by parsing template file
+        - unroll @{} into a vanilla tree (e.g. `<repeat>` instead of `foreach`)
+        - defer inlined properties & expressions to `process properties` step
+    - semantize: map node keys to known rendernode types
+    - fails upon malformed tree or unknown node types
+1. Process properties
+    - link properties to nodes of render tree
+    - first parse "stylesheet" properties
+    - semantize: map selectors to known template nodes+types, then property keys/values to known node properies + FromString=>values
+    - then override with inlined properties from template
+    - fails upon type mismatches, empty-set selectors, heterogenous multi-element selectors
+2. Process expressions
+    - parse & lambda-ize expressions, applying a la properties above
+    - return primitive types
+    - fails upon return type mismatch, malformed expression
  */
 
 fn main() {
     println!("Hello, world!");
-}   
+}
