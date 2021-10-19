33cb46419d1c97f3d174d05dee9dcef9b688d5bd
Zack Brown <zack@inclination.co>
2021-10-19 10:28:16 -0700

    wip: add pest JSON example to aid in setting up template parser

--- carbon-template-compiler/src/main.rs
+++ carbon-template-compiler/src/main.rs
@@ -1,3 +1,25 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+#[macro_use]
+extern crate lazy_static;
+
+use std::fs;
+
+
+use pest::Parser;
+use pest::prec_climber::PrecClimber;
+use pest::error::Error;
+
+// #[derive(Parser)]
+// #[grammar = "template.pest"]
+// pub struct TemplateParser;
+
+#[derive(Parser)]
+#[grammar = "json.pest"]
+pub struct JSONParser;
+
 
 /*
 
@@ -9,7 +31,19 @@ TODO:
     [ ] Expression parser
 
 */
-
+//
+// lazy_static! {
+//     static ref PREC_CLIMBER: PrecClimber<Rule> = {
+//         use Rule::*;
+//         use Assoc::*;
+//
+//         PrecClimber::new(vec![
+//             Operator::new(add, Left) | Operator::new(subtract, Left),
+//             Operator::new(multiply, Left) | Operator::new(divide, Left),
+//             Operator::new(power, Right)
+//         ])
+//     };
+// }
 
 
 /*
@@ -34,5 +68,95 @@ COMPILATION STAGES
  */
 
 fn main() {
-    println!("Hello, world!");
+
+    // let unparsed_file = fs::read_to_string("dev-samples/basic.dash").expect("cannot read file");
+    //
+    // let file = TemplateParser::parse(Rule::template, &unparsed_file)
+    //     .expect("unsuccessful parse") // unwrap the parse result
+    //     .next().unwrap(); // get and unwrap the `file` rule; never fails
+
+
+    let unparsed_file = fs::read_to_string("dev-samples/data.json").expect("cannot read file");
+
+    let json: JSONValue = parse_json_file(&unparsed_file).expect("unsuccessful parse");
+
+    println!("{}", serialize_jsonvalue(&json));
+
+
+}
+
+
+enum JSONValue<'a> {
+    Object(Vec<(&'a str, JSONValue<'a>)>),
+    Array(Vec<JSONValue<'a>>),
+    String(&'a str),
+    Number(f64),
+    Boolean(bool),
+    Null,
+}
+
+fn serialize_jsonvalue(val: &JSONValue) -> String {
+    use JSONValue::*;
+
+    match val {
+        Object(o) => {
+            let contents: Vec<_> = o
+                .iter()
+                .map(|(name, value)|
+                     format!("\"{}\":{}", name, serialize_jsonvalue(value)))
+                .collect();
+            format!("{{{}}}", contents.join(","))
+        }
+        Array(a) => {
+            let contents: Vec<_> = a.iter().map(serialize_jsonvalue).collect();
+            format!("[{}]", contents.join(","))
+        }
+        String(s) => format!("\"{}\"", s),
+        Number(n) => format!("{}", n),
+        Boolean(b) => format!("{}", b),
+        Null => format!("null"),
+    }
+}
+
+
+
+fn parse_json_file(file: &str) -> Result<JSONValue, Error<Rule>> {
+    let json = JSONParser::parse(Rule::json, file)?.next().unwrap();
+
+    use pest::iterators::Pair;
+
+    fn parse_value(pair: Pair<Rule>) -> JSONValue {
+        match pair.as_rule() {
+            Rule::object => JSONValue::Object(
+                pair.into_inner()
+                    .map(|pair| {
+                        let mut inner_rules = pair.into_inner();
+                        let name = inner_rules
+                            .next()
+                            .unwrap()
+                            .into_inner()
+                            .next()
+                            .unwrap()
+                            .as_str();
+                        let value = parse_value(inner_rules.next().unwrap());
+                        (name, value)
+                    })
+                    .collect(),
+            ),
+            Rule::array => JSONValue::Array(pair.into_inner().map(parse_value).collect()),
+            Rule::string => JSONValue::String(pair.into_inner().next().unwrap().as_str()),
+            Rule::number => JSONValue::Number(pair.as_str().parse().unwrap()),
+            Rule::boolean => JSONValue::Boolean(pair.as_str().parse().unwrap()),
+            Rule::null => JSONValue::Null,
+            Rule::json
+            | Rule::EOI
+            | Rule::pair
+            | Rule::value
+            | Rule::inner
+            | Rule::char
+            | Rule::WHITESPACE => unreachable!(),
+        }
+    }
+
+    Ok(parse_value(json))
 }
