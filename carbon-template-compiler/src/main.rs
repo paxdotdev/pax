7ee146c46336f107e1e92d4ce9727b9a74797c6a
Zack Brown <zack@inclination.co>
2021-10-19 11:38:46 -0700

    feat: documentation for grammar

--- carbon-template-compiler/src/main.rs
+++ carbon-template-compiler/src/main.rs
@@ -71,7 +71,7 @@ fn main() {
 
     let unparsed_file = fs::read_to_string("dev-samples/basic.dash").expect("cannot read file");
 
-    let file = TemplateParser::parse(Rule::template, &unparsed_file)
+    let file = TemplateParser::parse(Rule::dash_file, &unparsed_file)
         .expect("unsuccessful parse") // unwrap the parse result
         .next().unwrap(); // get and unwrap the `file` rule; never fails
 
