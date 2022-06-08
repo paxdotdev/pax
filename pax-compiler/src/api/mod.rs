bcf7ce5763c83dc940e7dd43f2b8e8c348c656f7
Zack Brown <zack@inclination.co>
2022-06-08 09:11:31 -0700

    wip

--- pax-compiler/src/api/mod.rs
+++ pax-compiler/src/api/mod.rs
@@ -363,25 +363,41 @@ fn recurse_visit_tag_pairs_for_template(ctx: &mut TemplateParseContext, any_tag_
         },
         Rule::statement_control_flow => {
 
-            //statement_control_flow => statement variant
             let matched_tag = any_tag_pair.into_inner().next().unwrap();
+            let new_id = create_uuid();
+
+            let mut pascal_identifier = "";
 
             match matched_tag.as_rule() {
                 Rule::statement_if => {
+                    pascal_identifier = "Conditional";
 
                 },
                 Rule::statement_for => {
-                    println!("Found for!");
+                    pascal_identifier = "Repeat";
+
                 },
                 Rule::statement_slot => {
+                    pascal_identifier = "Slot";
 
                 },
                 _ => {
                     unreachable!("Parsing error 883427242: {:?}", matched_tag.as_rule());;
                 }
             };
-            
-        }
+
+            let template_node = TemplateNodeDefinition {
+                id: new_id,
+                component_id: ctx.pascal_identifier_to_component_id_map.get(pascal_identifier).expect(&format!("Template key not found {}", &pascal_identifier)).to_string(),
+                inline_attributes: parse_inline_attribute_from_final_pairs_of_tag(open_tag),
+                children_ids: ctx.children_id_tracking_stack.pop().unwrap(),
+            };
+            ctx.template_node_definitions.push(template_node);
+
+        },
+        Rule::node_inner_content => {
+
+        },
         _ => {unreachable!("Parsing error 2232444421: {:?}", any_tag_pair.as_rule());}
     }
 }
@@ -599,85 +615,6 @@ pub fn parse_full_component_definition_string(mut ctx: ManifestContext, pax: &st
     (ctx, new_def)
 
 }
-
-//
-// enum JSONValue<'a> {
-//     Object(Vec<(&'a str, JSONValue<'a>)>),
-//     Array(Vec<JSONValue<'a>>),
-//     String(&'a str),
-//     Number(f64),
-//     Boolean(bool),
-//     Null,
-// }
-//
-// fn serialize_jsonvalue(val: &JSONValue) -> String {
-//     use JSONValue::*;
-//
-//     match val {
-//         Object(o) => {
-//             let contents: Vec<_> = o
-//                 .iter()
-//                 .map(|(name, value)|
-//                      format!("\"{}\":{}", name, serialize_jsonvalue(value)))
-//                 .collect();
-//             format!("{{{}}}", contents.join(","))
-//         }
-//         Array(a) => {
-//             let contents: Vec<_> = a.iter().map(serialize_jsonvalue).collect();
-//             format!("[{}]", contents.join(","))
-//         }
-//         String(s) => format!("\"{}\"", s),
-//         Number(n) => format!("{}", n),
-//         Boolean(b) => format!("{}", b),
-//         Null => format!("null"),
-//     }
-// }
-//
-//
-//
-// fn parse_json_file(file: &str) -> Result<JSONValue, Error<Rule>> {
-//     let json = JSONParser::parse(Rule::json, file)?.next().unwrap();
-//
-//     use pest::iterators::Pair;
-//
-//     fn parse_value(pair: Pair<Rule>) -> JSONValue {
-//         match pair.as_rule() {
-//             Rule::object => JSONValue::Object(
-//                 pair.into_inner()
-//                     .map(|pair| {
-//                         let mut inner_rules = pair.into_inner();
-//                         let name = inner_rules
-//                             .next()
-//                             .unwrap()
-//                             .into_inner()
-//                             .next()
-//                             .unwrap()
-//                             .as_str();
-//                         let value = parse_value(inner_rules.next().unwrap());
-//                         (name, value)
-//                     })
-//                     .collect(),
-//             ),
-//             Rule::array => JSONValue::Array(pair.into_inner().map(parse_value).collect()),
-//             Rule::string => JSONValue::String(pair.into_inner().next().unwrap().as_str()),
-//             Rule::number => JSONValue::Number(pair.as_str().parse().unwrap()),
-//             Rule::boolean => JSONValue::Boolean(pair.as_str().parse().unwrap()),
-//             Rule::null => JSONValue::Null,
-//             Rule::json
-//             | Rule::EOI
-//             | Rule::pair
-//             | Rule::value
-//             | Rule::inner
-//             | Rule::char
-//             | Rule::WHITESPACE => unreachable!(),
-//         }
-//     }
-//
-//     Ok(parse_value(json))
-// }
-
-
-
 
 
 
