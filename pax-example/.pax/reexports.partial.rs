846f862209fc5f1b6feaac980d4c4b1cb634182f
Warfa Jibril <warfaj@gmail.com>
2023-03-02 21:07:31 -0800

    Numeric working

--- pax-example/.pax/reexports.partial.rs
+++ pax-example/.pax/reexports.partial.rs
@@ -2,13 +2,16 @@ pub mod pax_reexports {
 	pub use crate::HelloRGB;
 	pub mod pax_std {
 		pub mod primitives {
+			pub use pax_std::primitives::Ellipse;
 			pub use pax_std::primitives::Group;
+			pub use pax_std::primitives::Path;
 			pub use pax_std::primitives::Rectangle;
 			pub use pax_std::primitives::Text;
 		}
 		pub mod types {
 			pub use pax_std::types::Color;
 			pub use pax_std::types::Font;
+			pub use pax_std::types::PathSegment;
 			pub use pax_std::types::Stroke;
 		}
 	}
