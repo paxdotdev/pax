52db3e8fb7d1024e65131ed29a51788b9141e4bb
Warfa Jibril <warfaj@gmail.com>
2023-03-29 13:23:58 -0700

    refactored run and implemented build

--- pax-chassis-web/pax-dev-harness-web/run-web.sh
+++ pax-chassis-web/pax-dev-harness-web/run-web.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-set -ex
+SHOULD_ALSO_RUN=$1
+OUTPUT_PATH=$2
 
-yarn serve || (yarn && yarn serve)
+# Clear old build and move to output directory
+rm -rf $OUTPUT_PATH
+mkdir -p $OUTPUT_PATH
+cp -r "." $OUTPUT_PATH
+cd $OUTPUT_PATH
+
+# Remove this script in output directory
+rm -- "$0"
+
+if [ "$SHOULD_ALSO_RUN" == "true" ]; then
+  # Run
+  set -ex
+  yarn serve || (yarn && yarn serve)
+fi
+
