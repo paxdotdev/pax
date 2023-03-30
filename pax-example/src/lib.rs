da74735ae8619924969b8abe10b6dc9e9e51cdf8
Warfa Jibril <warfaj@gmail.com>
2023-03-30 16:53:42 -0700

    implement in-line handlers (click+scroll)

--- pax-example/src/lib.rs
+++ pax-example/src/lib.rs
@@ -1,4 +1,4 @@
-use pax::api::{ArgsClick, ArgsRender, EasingCurve};
+use pax::api::{ArgsClick, ArgsRender, ArgsScroll, EasingCurve};
 use pax::*;
 use pax_std::components::Stacker;
 use pax_std::primitives::{Ellipse, Frame, Group, Path, Rectangle, Text};
@@ -7,21 +7,20 @@ use pax_std::primitives::{Ellipse, Frame, Group, Path, Rectangle, Text};
 #[pax_app(
     // for i in 0..5 {
     //    <Group transform={Transform2D::align(50%, 50%) * Transform2D::anchor(50%, 50%) * Transform2D::rotate(i * 0.07)} >
-            <Text text="Hello world" />
-            <Path />
-            <Ellipse fill={Color::rgb(0.5,0,1)} width=33.33% height=100% transform={
+            <Ellipse @click=self.handle_click fill={Color::rgb(0.5,0,1)} width=33.33% height=100% transform={
                 Transform2D::align(50%, 0%) * Transform2D::anchor(50%, 0%)
             } />
-            <Rectangle fill={Color::rgb(1,0.8,0.1)} width=33.33% height=100% transform={
+            <Rectangle @scroll=self.handle_scroll fill={Color::rgb(1,0.8,0.1)} width=33.33% height=100% transform={
                 Transform2D::align(100%, 0%) * Transform2D::anchor(100%, 0%)
             } />
+            <Text text="Hello world" />
+            <Path />
             <Rectangle fill={Color::rgb(0.25,0.5,0.5)} width=100% height=100% />
 
 
     @events {
-            click: self.handle_click,
-            prerender: [],
-            postrender: [handle_click, handle_click],
+            Click: [self.handle_global_click],
+            Scroll: self.handle_global_scroll,
             }
 
     //    </Group>
@@ -33,7 +32,16 @@ pub struct HelloRGB {
 
 impl HelloRGB {
     pub fn handle_click(&mut self, args: ArgsClick) {
-        log("sup");
+        log("click-ellipse");
+    }
+    pub fn handle_scroll(&mut self, args: ArgsScroll) {
+        log("scroll-rectangle");
+    }
+    pub fn handle_global_click(&mut self, args: ArgsClick) {
+        log("click-anywhere");
+    }
+    pub fn handle_global_scroll(&mut self, args: ArgsScroll) {
+        log("scroll-anywhere");
     }
 }
 
