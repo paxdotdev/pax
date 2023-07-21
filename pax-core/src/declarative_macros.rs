379a802dea587b11d50a2580d8ac3c686588f28b
Zack Brown <zack@pax.dev>
2023-07-21 10:58:43 +0700

    use std::mem::swap and drop our Box to fix the unsafe_unwrap memory leak

--- pax-core/src/declarative_macros.rs
+++ pax-core/src/declarative_macros.rs
@@ -7,30 +7,31 @@
 #[macro_export]
 macro_rules! unsafe_unwrap {
     ($source_enum:expr, $enum_type:ty, $target_type:ty) => {{
-        fn unwrap_impl<T, U>(source_enum: T) -> (U, *mut T) {
+        fn unwrap_impl<T, U: Default>(source_enum: T) -> U {
             let size_of_enum = std::mem::size_of::<T>();
             let size_of_target = std::mem::size_of::<U>();
             let align_of_enum = std::mem::align_of::<T>();
 
-            assert!(size_of_target <= size_of_enum, "The size_of target_type must be less than or equal to the size_of enum_type.");
+            assert!(size_of_target < size_of_enum, "The size_of target_type must be less than the size_of enum_type.");
 
-            let boxed_enum = Box::new(source_enum);
-            let enum_ptr = Box::into_raw(boxed_enum);
-            let ptr_to_text = unsafe { ((enum_ptr as *const u8).add(align_of_enum) as *const U) };
-            let target_copy = unsafe { ptr_to_text.read() };
+            let mut boxed_enum = Box::new(source_enum);
+            let mut default_value = U::default();
 
-            (target_copy, enum_ptr)
+            let target = unsafe {
+                let enum_ptr = Box::into_raw(boxed_enum);
+                let target_ptr = (enum_ptr as *mut u8).add(align_of_enum) as *mut U;
+
+                std::mem::swap(&mut *target_ptr, &mut default_value);
+
+                // We no longer need the boxed enum, so it can be safely dropped.
+                // Note that because the value inside the enum variant was replaced with a default value,
+                // dropping this box does not drop the original value.
+                drop(Box::from_raw(enum_ptr));
+
+                default_value
+            };
+            target
         }
         unwrap_impl::<$enum_type, $target_type>($source_enum)
-    }};
-}
-
-#[macro_export]
-macro_rules! unsafe_cleanup {
-    ($raw_ptr:expr, $type:ty) => {{
-        unsafe {
-            let _boxed = Box::from_raw($raw_ptr as *mut $type);
-            // The Box is immediately dropped here, deallocating the memory.
-        }
     }};
 }
