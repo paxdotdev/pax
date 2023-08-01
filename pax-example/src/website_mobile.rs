a866ea59acc547e91862fcd2713ff7a705f59c64
Warfa Jibril <warfaj@gmail.com>
2023-08-01 16:58:46 -0700

    refactored mobile template, fixed conditional issue, prod build setup

---
+++ pax-example/src/website_mobile.rs
@@ -0,0 +1,22 @@
+use pax_lang::*;
+use pax_lang::api::*;
+use pax_std::primitives::{Frame, Group, Rectangle, Text, Image};
+use pax_std::types::{Color, Fill, LinearGradient, StackerDirection};
+use pax_std::components::{Stacker, Sidebar};
+
+#[derive(Pax)]
+#[file("website_mobile.pax")]
+pub struct WebsiteMobile {
+    pub scroll_position: Property<f64>,
+}
+
+
+impl WebsiteMobile {
+    pub fn handle_container_scroll(&mut self, ctx: RuntimeContext, args: ArgsScroll) {
+        let mut scroll_position = *self.scroll_position.get();
+        scroll_position = scroll_position + args.delta_y;
+        scroll_position = scroll_position.min(0.0);
+        scroll_position = scroll_position.max(-3200.0);
+        self.scroll_position.set(scroll_position);
+    }
+}
