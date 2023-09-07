f355d1253a8486a8b9a250b510fecd3f02ad9929
Zack Brown <zack@pax.dev>
2023-09-07 15:08:18 -0700

    Release 0.6.15

--- pax-compiler/pax-create-template/src/lib.rs
+++ pax-compiler/pax-create-template/src/lib.rs
@@ -1,7 +1,7 @@
 use pax_lang::*;
 use pax_lang::api::*;
-use pax_std::primitives::{Frame, Group, Rectangle, Text, Image};
-use pax_std::types::{Color, Fill, LinearGradient, StackerDirection};
+use pax_std::primitives::{Group, Text};
+use pax_std::types::{Color};
 use pax_std::components::{Stacker, Sidebar};
 
 #[derive(Pax)]
