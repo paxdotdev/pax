e01f25485a7fd3e46c222e9807d2dc9f74febec1
Zack Brown <zack@pax.dev>
2023-09-13 12:15:47 -0700

    cargo fix

--- pax-cartridge/src/lib.rs
+++ pax-cartridge/src/lib.rs
@@ -1,10 +1,10 @@
 
 use std::cell::RefCell;
-use std::collections::{HashMap, VecDeque};
-use std::ops::Deref;
+use std::collections::{HashMap};
+
 use std::rc::Rc;
-use pax_core::{ComponentInstance, RenderNodePtr, PropertyExpression, RenderNodePtrList, RenderTreeContext, ExpressionContext, PaxEngine, RenderNode, InstanceRegistry, HandlerRegistry, InstantiationArgs, ConditionalInstance, SlotInstance, StackFrame};
-use pax_core::pax_properties_coproduct::{PropertiesCoproduct, TypesCoproduct};
+use pax_core::{ComponentInstance, ExpressionContext, InstanceRegistry, InstantiationArgs};
+use pax_core::pax_properties_coproduct::{TypesCoproduct};
 use piet_common::RenderContext;
 
 const PLACEHOLDER_ERROR : &str = "Fatal: the development placeholder cartridge is still attached -- a defined cartridge must be attached during compilation.  This means that Pax compilation failed -- please try again with `pax build` or `pax run`.";
@@ -13,10 +13,10 @@ pub fn instantiate_expression_table<R: 'static + RenderContext>() -> HashMap<usi
     unreachable!("{}", PLACEHOLDER_ERROR)
 }
 
-pub fn instantiate_component_stacker<R: 'static + RenderContext>(instance_registry: Rc<RefCell<InstanceRegistry<R>>>, mut args: InstantiationArgs<R>) -> Rc<RefCell<ComponentInstance<R>>>  {
+pub fn instantiate_component_stacker<R: 'static + RenderContext>(_instance_registry: Rc<RefCell<InstanceRegistry<R>>>, _args: InstantiationArgs<R>) -> Rc<RefCell<ComponentInstance<R>>>  {
     unreachable!("{}", PLACEHOLDER_ERROR)
 }
 
-pub fn instantiate_main_component<R: 'static + RenderContext>(instance_registry: Rc<RefCell<InstanceRegistry<R>>>) -> Rc<RefCell<ComponentInstance<R>>> {
+pub fn instantiate_main_component<R: 'static + RenderContext>(_instance_registry: Rc<RefCell<InstanceRegistry<R>>>) -> Rc<RefCell<ComponentInstance<R>>> {
     unreachable!("{}", PLACEHOLDER_ERROR)
 }
