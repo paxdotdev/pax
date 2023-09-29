d937b80c2da10d8d71a130a08cea4512c5c92bde
Zack Brown <zack@pax.dev>
2023-09-28 17:26:20 -0700

    add temp files for PoC dynamic build

---
+++ pax-chassis-web/interface/public/pax-chassis-web_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function wasm_memory(): number;
+export function __wbg_paxchassisweb_free(a: number): void;
+export function paxchassisweb_new(): number;
+export function paxchassisweb_add_context(a: number, b: number, c: number): void;
+export function paxchassisweb_send_viewport_update(a: number, b: number, c: number): void;
+export function paxchassisweb_remove_context(a: number, b: number, c: number): void;
+export function paxchassisweb_interrupt(a: number, b: number, c: number, d: number): void;
+export function paxchassisweb_deallocate(a: number, b: number): void;
+export function paxchassisweb_tick(a: number): number;
+export function __wbg_memoryslice_free(a: number): void;
+export function memoryslice_ptr(a: number): number;
+export function memoryslice_len(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
