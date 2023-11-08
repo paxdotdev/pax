3b607414915d120124ea3b72dc5d2976793c7f88
Zack Brown <zack@pax.dev>
2023-11-07 16:10:20 -0800

    wip: rebuilding engine; hooking up control flow

--- pax-core/src/conditional.rs
+++ pax-core/src/conditional.rs
@@ -1,7 +1,7 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use crate::{InstantiationArgs, InstanceNode, InstanceNodePtr, InstanceNodePtrList, RenderTreeContext, ExpandedNode, PropertiesTreeContext, with_properties_unsafe, unsafe_unwrap, unsafe_wrap, handle_vtable_update};
+use crate::{InstantiationArgs, InstanceNode, InstanceNodePtr, InstanceNodePtrList, RenderTreeContext, ExpandedNode, PropertiesTreeContext, with_properties_unsafe, unsafe_unwrap, unsafe_wrap, handle_vtable_update, recurse_compute_properties};
 use pax_properties_coproduct::{ConditionalProperties, PropertiesCoproduct, TypesCoproduct};
 use pax_runtime_api::{CommonProperties, Layer, PropertyInstance, Size};
 use piet_common::RenderContext;
@@ -54,57 +54,30 @@ impl<R: 'static + RenderContext> InstanceNode<R> for ConditionalInstance<R> {
     fn handle_compute_properties(&mut self, ptc: &mut PropertiesTreeContext<R>) -> Rc<RefCell<ExpandedNode<R>>> {
 
         // evaluate boolean expression
-        let properties_wrapped =  ptc.current_expanded_node.as_ref().unwrap().borrow().get_properties();
+        let this_expanded_node = ExpandedNode::get_or_create_with_prototypical_properties(ptc, &self.instance_prototypical_properties, &self.instance_prototypical_common_properties);
+        let properties_wrapped =  this_expanded_node.borrow().get_properties();
 
         let evaluated_condition = with_properties_unsafe!(&properties_wrapped, PropertiesCoproduct, ConditionalProperties, |properties: &mut ConditionalProperties| {
-
             handle_vtable_update!(ptc, properties.boolean_expression, bool);
-            if let Some(id) = properties.boolean_expression._get_vtable_id() {
-                let new_value = ptc.compute_vtable_value(id);
-                if let TypesCoproduct::bool(val) = new_value {
-                    properties.boolean_expression.set(val);
-                    val
-                } else {
-                    unreachable!(); //Conditional's condition return type is expected to be boolean
-                }
-            } else {
-                unreachable!() //Conditional's boolean expression must be an expression, but a non-expression value was found
-            }
+            //return evaluated value
+            *properties.boolean_expression.get()
         });
 
-        if evaluated_condition {
-            // if true, recurse into instance children, stitch ExpandedNode subtree and return subtree root (this_expanded_node)
-        } else {
-            // else, just return self (ExpandedNode without any ExpandedNode children)
+        // if true, recurse into instance children, stitch ExpandedNode subtree and return subtree root (this_expanded_node)
+        for child in self.instance_children.borrow().iter() {
+            let mut new_ptc = ptc.clone();
+            new_ptc.current_expanded_node = None;
+            new_ptc.current_instance_node = Rc::clone(child);
 
+            if !evaluated_condition {
+                new_ptc.marked_for_unmount = true
+            }
+
+            let expanded_child = recurse_compute_properties(&mut new_ptc);
         }
 
-        // if let Some(boolean_expression) =
-        //     ptc.compute_vtable_value(self.boolean_expression._get_vtable_id())
-        // {
-        //     let old_value = *self.boolean_expression.get();
-        //     let new_value = if let TypesCoproduct::bool(v) = boolean_expression {
-        //         v
-        //     } else {
-        //         unreachable!()
-        //     };
-        //
-        //     let mut node_registry = (*ptc.engine.node_registry).borrow_mut();
-        //     if old_value && !new_value {
-        //         (*self.true_branch_children)
-        //             .borrow_mut()
-        //             .iter()
-        //             .for_each(|child| {
-        //                 let instance_id = (*(*child)).borrow_mut().get_instance_id();
-        //                 node_registry.deregister(instance_id);
-        //                 node_registry.mark_for_unmount(instance_id);
-        //             });
-        //         self.cleanup_children = self.true_branch_children.clone();
-        //     }
-        //     self.boolean_expression.set(new_value);
-        // }
+        this_expanded_node
 
-        todo!()
     }
 
     fn is_invisible_to_slot(&self) -> bool {
