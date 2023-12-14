678cdcbf519355355ececa16f582325fdbc29dad
Zack Brown <zack@pax.dev>
2023-12-13 23:07:46 -0800

    designer broad strokes

--- pax-designer/pax-designer/src/lib.rs
+++ pax-designer/pax-designer/src/lib.rs
@@ -2,33 +2,17 @@
 
 use pax_lang::*;
 use pax_lang::api::*;
-use pax_std::primitives::*;
-use pax_std::types::*;
-use pax_std::types::text::*;
-use pax_std::components::*;
-use pax_std::components::Stacker;
+
+pub mod hud;
+pub mod controls;
+
+use crate::hud::Hud;
+use crate::controls::Controls;
+
+//TODO: import userland project as `Renderer` for `<Renderer />`
 
 #[derive(Pax)]
 #[main]
 #[file("lib.pax")]
-pub struct Example {
-    pub ticks: Property<usize>,
-    pub num_clicks: Property<usize>,
-    pub message: Property<String>,
-}
+pub struct PaxDesigner {}
 
-impl Example {
-    pub fn handle_mount(&mut self, ctx: &NodeContext) {
-        self.message.set("Click me".to_string());
-    }
-    pub fn handle_pre_render(&mut self, ctx: &NodeContext) {
-        let old_ticks = self.ticks.get();
-        self.ticks.set(old_ticks + 1);
-    }
-
-    pub fn increment(&mut self, ctx: &NodeContext, args: ArgsClick){
-        let old_num_clicks = self.num_clicks.get();
-        self.num_clicks.set(old_num_clicks + 1);
-        self.message.set(format!("{} clicks", self.num_clicks.get()));
-    }
-}
