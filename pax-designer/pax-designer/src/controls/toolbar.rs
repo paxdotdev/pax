9e328bbe549e69a4fed56b6e274b25f3a4c67ebe
Zack Brown <zack@pax.dev>
2023-12-22 14:15:39 -0800

    wip: safety, toward toolbar

--- pax-designer/pax-designer/src/controls/toolbar.rs
+++ pax-designer/pax-designer/src/controls/toolbar.rs
@@ -1,6 +1,12 @@
 
 use pax_lang::*;
+use crate::controls::toolbar_panel::ToolbarPanel;
+use pax_std::primitives::{Rectangle, Group};
 
 #[derive(Pax)]
 #[file("controls/toolbar.pax")]
-pub struct Toolbar {}
+pub struct Toolbar {
+    // panels: Property<Vec<ToolbarPanel>>
+}
+
+
