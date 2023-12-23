976e65037bb9d12766164b4c5c406e6ffb885fe3
Zack Brown <zack@pax.dev>
2023-12-22 16:52:56 -0800

    toward designer UI

--- pax-designer/pax-designer/src/controls/logobar.rs
+++ pax-designer/pax-designer/src/controls/logobar.rs
@@ -1,6 +1,8 @@
 use pax_lang::*;
 use pax_lang::api::*;
 
+use pax_std::primitives::Text;
+
 #[derive(Pax)]
 #[file("controls/logobar.pax")]
 pub struct Logobar {
--- pax-designer/pax-designer/src/controls/toolbar.rs
+++ pax-designer/pax-designer/src/controls/toolbar.rs
@@ -1,12 +1,30 @@
-
+use std::collections::HashMap;
+use std::rc::Rc;
 use pax_lang::*;
-use crate::controls::toolbar_panel::ToolbarPanel;
-use pax_std::primitives::{Rectangle, Group};
+use pax_lang::api::{ArgsClick, NodeContext};
+use pax_std::primitives::{Rectangle, Group, Text};
+
 
 #[derive(Pax)]
 #[file("controls/toolbar.pax")]
 pub struct Toolbar {
-    // panels: Property<Vec<ToolbarPanel>>
 }
+
+impl Toolbar {
+
+    pub fn handle_click_pointer(&mut self, ctx: &NodeContext, args: ArgsClick) {
+        unimplemented!("handle click for pointer")
+    }
+
+    pub fn handle_click_brush(&mut self, ctx: &NodeContext, args: ArgsClick) {
+        unimplemented!("handle click for brush")
+    }
+
+    pub fn handle_click_pen(&mut self, ctx: &NodeContext, args: ArgsClick) {
+        unimplemented!("handle click for pen")
+    }
+}
+
+
 
 
