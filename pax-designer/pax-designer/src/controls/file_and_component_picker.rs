72d2b2fe1b31e18f54f7c30f0e9bff8ddee07822
Zack Brown <zack@pax.dev>
2023-12-22 16:52:56 -0800

    toward designer UI

--- pax-designer/pax-designer/src/controls/file_and_component_picker.rs
+++ pax-designer/pax-designer/src/controls/file_and_component_picker.rs
@@ -1,6 +1,8 @@
 use pax_lang::*;
 use pax_lang::api::*;
 
+use pax_std::primitives::Text;
+
 #[derive(Pax)]
 #[file("controls/file_and_component_picker.pax")]
 pub struct FileAndComponentPicker {}
--- pax-designer/pax-designer/src/controls/tree.rs
+++ pax-designer/pax-designer/src/controls/tree.rs
@@ -1,5 +1,7 @@
 
 use pax_lang::*;
+use pax_std::primitives::Text;
+
 
 #[derive(Pax)]
 #[file("controls/tree.pax")]
