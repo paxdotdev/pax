4d9b40e49a1667b59520d99d08840075ef97aaa0
Zack Brown <zack@pax.dev>
2024-01-11 17:45:33 -0800

    cargo fix

--- pax-designer/pax-designer/src/controls/toolbar.rs
+++ pax-designer/pax-designer/src/controls/toolbar.rs
@@ -11,35 +11,35 @@ pub struct Toolbar {
 }
 
 impl Toolbar {
-    pub fn handle_click_pointer(&mut self, ctx: &NodeContext, args: ArgsClick) {
+    pub fn handle_click_pointer(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
         unimplemented!("handle click for pointer")
     }
 
-    pub fn handle_click_brush(&mut self, ctx: &NodeContext, args: ArgsClick) {
+    pub fn handle_click_brush(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
         unimplemented!("handle click for brush")
     }
 
-    pub fn handle_click_pen(&mut self, ctx: &NodeContext, args: ArgsClick) {
+    pub fn handle_click_pen(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
         unimplemented!("handle click for pen")
     }
 
-    pub fn handle_click_rect(&mut self, ctx: &NodeContext, args: ArgsClick) {
+    pub fn handle_click_rect(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
         unimplemented!("handle click for rect")
     }
 
-    pub fn handle_click_stacker(&mut self, ctx: &NodeContext, args: ArgsClick) {
+    pub fn handle_click_stacker(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
         unimplemented!("handle click for stacker")
     }
 
-    pub fn handle_click_text(&mut self, ctx: &NodeContext, args: ArgsClick) {
+    pub fn handle_click_text(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
         unimplemented!("handle click for text")
     }
 
-    pub fn handle_click_checkbox(&mut self, ctx: &NodeContext, args: ArgsClick) {
+    pub fn handle_click_checkbox(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
         unimplemented!("handle click for checkbox")
     }
 
-    pub fn handle_click_speech(&mut self, ctx: &NodeContext, args: ArgsClick) {
+    pub fn handle_click_speech(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
         unimplemented!("handle click for speech")
     }
 }
