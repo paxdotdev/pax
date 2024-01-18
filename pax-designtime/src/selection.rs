9dcf5409f7a483c4b879b2ed1c2a5acbaad9ece2
Zack Brown <zack@pax.dev>
2024-01-17 20:27:33 -0800

    todo

--- pax-designtime/src/selection.rs
+++ pax-designtime/src/selection.rs
@@ -1,5 +1,8 @@
 
 
 pub struct PaxSelectionManager {
-    
+    //keep action stack
+    //elsewhere in logic, something may register itself as an undoable action (e.g. selection, or manifest ORM)
+    //for certain actions, trigger upstream ORM actions (undo an action by ID, or keep track of stack state/status and undo tactically)
+    //for other actions (like selection), manage undo/redo separately
 }
