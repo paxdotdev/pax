3a9f757cfdb941b52a4b17919f441c8c1a58dc81
Samuel Selleck <samuel@pax.dev>
2024-01-17 16:25:29 -0800

    textboxes in settings view

--- pax-designer/pax-designer/src/controls/settings/property_editor.rs
+++ pax-designer/pax-designer/src/controls/settings/property_editor.rs
@@ -10,9 +10,27 @@ use pax_std::types::*;
 #[file("controls/settings/property_editor.pax")]
 pub struct PropertyEditor {
     pub name: Property<StringBox>,
+    pub definition: Property<String>,
     pub def: Property<String>,
+    pub init: Property<i32>,
 }
 
 impl PropertyEditor {
-    pub fn on_mount(&mut self, ctx: &NodeContext) {}
+    pub fn on_mount(&mut self, ctx: &NodeContext) {
+        self.def.set(self.definition.get().clone());
+    }
+
+    pub fn on_render(&mut self, ctx: &NodeContext) {
+        self.init.set(self.init.get() + 1);
+        // Need to wait a bit before we initialize def, so that self.definition has been
+        // populated with it's default value.
+        if *self.init.get() == 2 {
+            log("INIT!!");
+            self.def.set(self.definition.get().clone());
+        }
+    }
+
+    pub fn text_change(&mut self, ctx: &NodeContext, args: ArgsTextboxChange) {
+        self.def.set(args.text);
+    }
 }
