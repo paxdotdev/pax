c35bdacf35ec2cb8c2868fe85b373ba85dff84cd
Warfa Jibril <warfaj@gmail.com>
2024-01-22 22:51:23 -0800

    build pax-designer with designtime cli

--- pax-designer/pax-designer/run.sh
+++ pax-designer/pax-designer/run.sh
@@ -3,11 +3,10 @@
 # Path to the pax-cli file
 PAX_CLI="../pax/target/debug/pax-cli"
 
-# Check if the pax-cli file exists
-if [ ! -f "$PAX_CLI" ]; then
-    echo "Warning: $PAX_CLI does not exist. You must first build pax-cli in the context of the ../pax directory (cargo build --workspace))"
-    exit 1
-else
-    # Existing command from your script
-    PAX_WORKSPACE_ROOT=../pax $PAX_CLI run --target=web --libdev --verbose
-fi
+# Build pax-cli with designtime flag
+pushd ../pax/pax-cli
+cargo build --features="designtime"
+popd
+
+# Existing command from your script
+PAX_WORKSPACE_ROOT=../pax $PAX_CLI run --target=web --libdev --verbose
