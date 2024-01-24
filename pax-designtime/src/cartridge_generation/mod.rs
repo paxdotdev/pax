8ef8af832d9474c7c37c552a0b1d063a9620421d
Warfa Jibril <warfaj@gmail.com>
2024-01-24 15:51:55 -0800

    migrated hostcrateinfo

--- pax-designtime/src/cartridge_generation/mod.rs
+++ pax-designtime/src/cartridge_generation/mod.rs
@@ -1,4 +1,5 @@
 use include_dir::{include_dir, Dir};
+use pax_manifest::{HostCrateInfo, PaxManifest};
 #[allow(unused_imports)]
 use serde_derive::{Deserialize, Serialize};
 #[allow(unused_imports)]
@@ -8,6 +9,15 @@ use tera::{Context, Tera};
 #[allow(unused)]
 static TEMPLATE_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/templates/cartridge_generation");
 static DESIGN_CARTRIDGE_TEMPLATE: &str = "designtime-cartridge.tera";
+
+
+pub fn generate_designtime_cartridge(manifest: PaxManifest, host_crate_info: &HostCrateInfo) -> String {
+    let args = TemplateArgsCodegenDesigntimeCartridge {};
+    press_template_codegen_designtime_cartridge(args)
+}
+
+
+
 
 #[derive(Serialize)]
 pub struct TemplateArgsCodegenDesigntimeCartridge {}
