d47ef0be3fa9d6785d9ca7a79390482fe515a74c
Samuel Selleck <samuel@pax.dev>
2024-01-24 13:27:49 -0800

    dup functions left from rebase, test + designer working now

--- pax-designtime/src/priveleged_agent.rs
+++ pax-designtime/src/priveleged_agent.rs
@@ -58,32 +58,4 @@ impl PrivilegedAgentConnection {
         self.sender.send(ewebsock::WsMessage::Binary(msg_bytes));
         Ok(())
     }
-
-    // Something similar to this can eventually be used to recieve wasm reload requests
-    pub fn _tick_recv(&mut self) {
-        if let Some(value) = self.recver.try_recv() {
-            match value {
-                ewebsock::WsEvent::Message(ewebsock::WsMessage::Binary(bytes)) => {
-                    let Ok(res) = rmp_serde::from_slice(&bytes) else {
-                        return;
-                    };
-                    match res {
-                        AgentMessage::ManifestSerializationAcknowledgement(ack) => {
-                            log::info!("manifest serialization acc id: {}", ack.id)
-                        }
-                        _ => {
-                            log::warn!("recieved unexpected acc after sending manifest update")
-                        }
-                    }
-                }
-                ewebsock::WsEvent::Opened => log::info!("opened????"),
-                ewebsock::WsEvent::Error(_) => log::info!("unexpected: error"),
-                ewebsock::WsEvent::Closed => log::info!("unexpected: closed connection"),
-                _ => log::warn!("whaat!!"),
-            }
-        } else {
-            log::warn!("no message!!");
-            std::thread::sleep(std::time::Duration::from_millis(10));
-        }
-    }
 }
