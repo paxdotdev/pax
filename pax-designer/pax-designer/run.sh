6edec10b068d5a360f378bdde9f1a491ea4b46cf
Warfa Jibril <warfaj@gmail.com>
2024-01-23 18:11:41 -0800

    beefed up pax-designer run script to handle designtime deps and added runtime cargo manipulation for designtime to its crate

--- pax-designer/pax-designer/run.sh
+++ pax-designer/pax-designer/run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+source helpers.sh
 
 # Path to the pax-cli file
 PAX_CLI="../pax/target/debug/pax-cli"
+
+add_designtime_dependency
 
 # Build pax-cli with designtime flag
 pushd ../pax/pax-cli
 cargo build --features="designtime"
 popd
 
-# Existing command from your script
-PAX_WORKSPACE_ROOT=../pax $PAX_CLI run --target=web --libdev --verbose
+remove_designtime_dependency
+
+# Run cli
+PAX_WORKSPACE_ROOT=../pax PAX_CORP_ROOT=../ $PAX_CLI run --target=web --libdev --verbose
