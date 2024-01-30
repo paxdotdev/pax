3b640475cb094aa2a7c4ba914b254a623a81e44b
Zack Brown <zack@pax.dev>
2024-01-30 12:09:09 -0800

    wip: toward glass UI and selection state; hook up mouseup/down/move triggers for glass

--- pax-designtime/src/selection.rs
+++ pax-designtime/src/selection.rs
@@ -1,12 +1,8 @@
+use uuid::Uuid;
 
 pub struct SelectionManager {
     selection_state: SelectionState,
-
-    //TODO: subscription is tricky due to move closures + ownership.  Either figure out an approach to fine-grained moves
-    // (e.g. of cloneable property channels, or cloneable property Rc<RefCell<>>s), or make this a brute-force, per-tick check instead of a reactive subscription mechanism.
-    // One hybrid approach could be to keep a fingerprint, e.g. a UUID per change.  A "subscriber" can determine that selection state has changed
-    // by checking that fingerprint each tick.  The subscriber must update IFF that UUID is different than the subscriber's locally stored copy.
-    selection_subscribers: Vec<Box<dyn FnMut(&SelectionState)>>,
+    last_mutation_id: Uuid,
 }
 
 impl Default for SelectionManager {
@@ -20,31 +16,10 @@ impl Default for SelectionManager {
 pub type SelectionState = Vec<usize>;
 
 impl SelectionManager {
-    fn new() -> Self {
+    pub fn new() -> Self {
         SelectionManager {
-            selection_subscribers: Vec::new(),
             selection_state: SelectionState::default(),
-        }
-    }
-
-
-
-    //in glass
-    pub fn handle_pre_tick(&mut self, dt: Rc<RefCell<DesignTimeManager>>) {
-        self.
-    }
-
-
-
-    fn subscribe<F>(&mut self, callback: F)
-        where F: FnMut(&SelectionState) + 'static {
-        self.selection_subscribers.push(Box::new(callback));
-    }
-
-    fn notify_subscribers(&mut self) {
-        let state = self.selection_state.clone();
-        for subscriber in &mut self.selection_subscribers {
-            subscriber(&state);
+            last_mutation_id: Uuid::new_v4(),
         }
     }
 }
