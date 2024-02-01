d82dead8a2a45f837ea0bbd58df6a23a3edbfebf
Samuel Selleck <samuel@pax.dev>
2024-02-01 12:49:31 -0800

    Start privelegent agent on pax-designer run. Serialize component button in designer

--- pax-designtime/src/priveleged_agent.rs
+++ pax-designtime/src/priveleged_agent.rs
@@ -1,8 +1,9 @@
 use std::net::SocketAddr;
 
-use crate::messages::{AgentMessage, ManifestSerializationRequest};
+use crate::messages::{AgentMessage, ComponentSerializationRequest, ManifestSerializationRequest};
 use anyhow::{anyhow, Result};
-use pax_manifest::PaxManifest;
+use pax_manifest::{ComponentDefinition, PaxManifest};
+
 pub struct PrivilegedAgentConnection {
     sender: ewebsock::WsSender,
     _recver: ewebsock::WsReceiver,
@@ -18,12 +19,10 @@ impl PrivilegedAgentConnection {
         })
     }
 
-    pub fn send_manifest_update(&mut self, manifest: &PaxManifest) -> Result<()> {
-        let manifest_bytes = rmp_serde::to_vec(&manifest)?;
-        let msg_bytes = rmp_serde::to_vec(&AgentMessage::ManifestSerializationRequest(
-            ManifestSerializationRequest {
-                manifest: manifest_bytes,
-            },
+    pub fn send_component_update(&mut self, component: &ComponentDefinition) -> Result<()> {
+        let component_bytes = rmp_serde::to_vec(&component)?;
+        let msg_bytes = rmp_serde::to_vec(&AgentMessage::ComponentSerializationRequest(
+            ComponentSerializationRequest { component_bytes },
         ))?;
 
         self.sender.send(ewebsock::WsMessage::Binary(msg_bytes));
