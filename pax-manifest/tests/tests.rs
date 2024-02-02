469f2d69d2b91edf6b6a6592f4ab5b4ff948f1a5
Samuel Selleck <samuel@pax.dev>
2024-02-02 12:53:01 -0800

    parsing tests in pax-manifest

---
+++ pax-manifest/tests/tests.rs
@@ -0,0 +1,47 @@
+#[cfg(test)]
+#[cfg(feature = "parsing")]
+mod tests {
+
+    use pax_manifest::{utils, ValueDefinition};
+
+    #[test]
+    fn test_parse_embty() {
+        assert!(matches!(utils::parse_value(""), Ok(_)));
+    }
+
+    #[test]
+    fn test_parse_identifier() {
+        let res = utils::parse_value("identifier");
+        if let Ok(ValueDefinition::Identifier(token, _)) = res {
+            assert_eq!(&token.raw_value, "identifier");
+        } else {
+            panic!("unexpected result: {:?}", res);
+        }
+    }
+
+    #[test]
+    fn test_parse_literal_number() {
+        let res = utils::parse_value("5");
+        if let Ok(ValueDefinition::LiteralValue(token)) = res {
+            assert_eq!(&token.raw_value, "5");
+        } else {
+            panic!("unexpected result: {:?}", res);
+        }
+    }
+
+    #[test]
+    fn test_parse_expression() {
+        let res = utils::parse_value("{5 + 3}");
+        if let Ok(ValueDefinition::Expression(token, _)) = res {
+            assert_eq!(&token.raw_value, "{5 + 3}");
+        } else {
+            panic!("unexpected result: {:?}", res);
+        }
+    }
+
+    #[test]
+    fn test_parse_with_extra() {
+        let res = utils::parse_value("{5 + 3}this_shouldn't succeed");
+        assert!(matches!(res, Err(_)));
+    }
+}
