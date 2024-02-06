01b7e95558ed44fa4ac46a0bbe6247557566c150
Samuel Selleck <samuel@pax.dev>
2024-02-05 16:27:03 -0800

    remove custom(Import)

--- examples/src/three-repeats/src/lib.rs
+++ examples/src/three-repeats/src/lib.rs
@@ -1,12 +1,12 @@
 #![allow(unused_imports)]
 
-use pax_lang::*;
 use pax_lang::api::*;
-use pax_std::primitives::*;
-use pax_std::types::*;
-use pax_std::types::text::*;
-use pax_std::components::*;
+use pax_lang::*;
 use pax_std::components::Stacker;
+use pax_std::components::*;
+use pax_std::primitives::*;
+use pax_std::types::text::*;
+use pax_std::types::*;
 
 #[pax]
 #[main]
@@ -20,28 +20,17 @@ impl Default for ThreeRepeats {
     fn default() -> Self {
         Self {
             some_data: Box::new(PropertyLiteral::new(vec![
-                CustomStruct{
-                    x: 250
-                },
-                CustomStruct{
-                    x: 300
-                },
-                CustomStruct{
-                    x: 450
-                },
-                CustomStruct{
-                    x: 550
-                },
-                CustomStruct{
-                    x: 850
-                }
-            ]))
+                CustomStruct { x: 250 },
+                CustomStruct { x: 300 },
+                CustomStruct { x: 450 },
+                CustomStruct { x: 550 },
+                CustomStruct { x: 850 },
+            ])),
         }
     }
 }
 
 #[pax]
-#[custom(Imports)]
 pub struct CustomStruct {
     pub x: isize,
 }
