4e2e894025bccf0cf3f45891fcde2a0bb8246edf
Zack Brown <zack@pax.dev>
2024-02-09 15:57:17 -0800

    cargo fix

--- pax-runtime/src/engine/mod.rs
+++ pax-runtime/src/engine/mod.rs
@@ -179,7 +179,7 @@ pub struct ExpressionTable {
 
 #[cfg(debug_assertions)]
 impl Debug for ExpressionTable {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
         unimplemented!()
     }
 }
