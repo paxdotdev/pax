27ae0263eb499c9ba0ebce7c0b9c98d28467535e
Warfa Jibril <warfaj@gmail.com>
2024-02-11 00:36:15 -0800

    designer running again

--- pax-designer/pax-designer/run.sh
+++ pax-designer/pax-designer/run.sh
@@ -13,9 +13,9 @@ popd
 
 remove_designtime_dependency
 
-# Run cli & priv agent in parallell, and pipe both outputs to terminal
-(PAX_WORKSPACE_ROOT=../pax PAX_CORP_ROOT=../ $PAX_CLI run --target=web --libdev --verbose) \
-2>&1 | tee >(cd ../pax-privileged-agent && cargo run -- ../designer-project)
+# Run cli & priv agent in parallel, and pipe both outputs to terminal
+(cd ../pax-privileged-agent && cargo run -- ../designer-project) \
+2>&1 | tee >(PAX_WORKSPACE_ROOT=../pax PAX_CORP_ROOT=../ $PAX_CLI run --target=web --libdev --verbose)
 
 #terminate the priv agent if it for some reason is still alive
-kill -15 $(lsof -t -i:8252) > /dev/null & 
+lsof -ti:8252 | xargs -r kill -15
