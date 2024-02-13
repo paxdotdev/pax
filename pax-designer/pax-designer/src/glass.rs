3a1d3d154ae855f22adfd8e6ceaec02c87089308
Samuel Selleck <samuel@pax.dev>
2024-02-13 13:09:00 -0800

    selection state working

--- pax-designer/pax-designer/src/glass.rs
+++ pax-designer/pax-designer/src/glass.rs
@@ -70,8 +70,26 @@ impl Glass {
         model::read_app_state(|app_state| {
             if let Some(id) = app_state.selected_template_node_id {
                 self.selection_active.set(true);
-                let points = app_state.TEMP_TODO_REMOVE_bounds;
-                let sv = SelectionVisual::new_from_box_bounds(points);
+                let bounds = ctx
+                    .runtime_context
+                    .get_expanded_nodes_by_global_ids(&app_state.selected_component_id, id)
+                    .into_iter()
+                    .flat_map(|n| {
+                        let lp = n.layout_properties.borrow();
+                        lp.as_ref().map(|c| c.computed_tab.corners())
+                    })
+                    .collect();
+                let bounds = compute_total_bounds(bounds);
+                let mut sv = SelectionVisual::new_from_box_bounds(bounds);
+
+                // HACK before dirty-dag (to make sure repeat updates)
+                if self.control_points.get().len() == sv.control_points.len() {
+                    sv.control_points.push(ControlPoint {
+                        x: f64::MIN,
+                        y: f64::MIN,
+                    });
+                    sv.bounding_segments.push(BoundingSegment::default());
+                }
                 self.control_points.set(sv.control_points);
                 self.anchor_point.set(sv.anchor_point);
                 self.bounding_segments.set(sv.bounding_segments);
@@ -202,4 +220,27 @@ pub struct RectTool {
     pub height: Size,
     pub fill: Color,
     pub stroke: Color,
+}
+
+fn compute_total_bounds(bounds: Vec<[Point2D; 4]>) -> [Point2D; 4] {
+    let mut min_x = f64::MAX;
+    let mut min_y = f64::MAX;
+    let mut max_x = f64::MIN;
+    let mut max_y = f64::MIN;
+    for bound in bounds {
+        for p in bound {
+            min_x = min_x.min(p.x);
+            max_x = max_x.max(p.x);
+            min_y = min_y.min(p.y);
+            max_y = max_y.max(p.y);
+        }
+    }
+
+    let points = [
+        Point2D { x: min_x, y: min_y },
+        Point2D { x: min_x, y: max_y },
+        Point2D { x: max_x, y: max_y },
+        Point2D { x: max_x, y: min_y },
+    ];
+    points
 }
