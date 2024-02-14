dc003bbcc787bf1fd75564bc54966bd5c04e1400
Zack Brown <zack@pax.dev>
2024-02-13 18:49:57 -0800

    more design tweaks

--- pax-designer/pax-designer/src/controls/logobar.rs
+++ pax-designer/pax-designer/src/controls/logobar.rs
@@ -6,3 +6,12 @@ use pax_std::primitives::{Text, Image};
 #[pax]
 #[file("controls/logobar.pax")]
 pub struct Logobar {}
+
+
+impl Logobar {
+    pub fn handle_logo_click(&mut self, ctx: &NodeContext, _args: ArgsClick) {
+        let mut dt = ctx.designtime.borrow_mut();
+        dt.send_component_update("pax_designer::pax_reexports::designer_project::Example");
+    }
+
+}
