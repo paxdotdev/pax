fd816afc2e579d94f67107e9faec9a2b4e532510
Samuel Selleck <samuel@pax.dev>
2024-02-13 17:54:57 -0800

    settings view shows properties (can not be edited)

--- pax-designer/pax-designer/src/controls/settings/mod.rs
+++ pax-designer/pax-designer/src/controls/settings/mod.rs
@@ -13,12 +13,13 @@ use serde::Deserialize;
 pub mod property_editor;
 use property_editor::PropertyEditor;
 
+use crate::model;
+
 #[pax]
 #[file("controls/settings/settings.pax")]
 pub struct Settings {
     pub component_selected: Property<bool>,
     pub selected_component_name: Property<String>,
-
     // common props
     pub pos_x: Property<StringBox>,
     pub pos_y: Property<StringBox>,
@@ -51,77 +52,58 @@ pub struct PropertyDef {
 impl Settings {
     pub fn on_mount(&mut self, _ctx: &NodeContext) {}
 
-    pub fn set_object1(&mut self, _ctx: &NodeContext, _args: ArgsButtonClick) {
-        self.component_selected.set(true);
-        self.stid.set(StringBox::from(
-            "pax_designer::pax_reexports::controls::settings::Settings".to_owned(),
-        ));
-        self.snid.set(5.into());
-    }
-
-    pub fn set_object2(&mut self, _ctx: &NodeContext, _args: ArgsButtonClick) {
-        self.component_selected.set(true);
-        self.stid.set(StringBox::from(
-            "pax_designer::pax_reexports::designer_project::Example".to_owned(),
-        ));
-        self.snid.set(2.into());
-    }
-
-    pub fn set_object3(&mut self, _ctx: &NodeContext, _args: ArgsButtonClick) {
-        self.component_selected.set(true);
-        self.stid.set(StringBox::from(
-            "pax_designer::pax_reexports::designer_project::Example".to_owned(),
-        ));
-        self.snid.set(1.into());
-    }
-
     pub fn pre_render(&mut self, ctx: &NodeContext) {
-        if *self.component_selected.get() {
-            self.set_to_selected(ctx);
-        }
-    }
+        model::read_app_state(|app_state| {
+            let Some(temp_node_id) = app_state.selected_template_node_id else {
+                self.component_selected.set(false);
+                return;
+            };
+            let type_id = &app_state.selected_component_id;
 
-    pub fn set_to_selected(&mut self, ctx: &NodeContext) {
-        let type_id = &self.stid.get().string;
-        let temp_node_id = self.snid.get().get_as_int() as usize;
-        let (properties, type_name) = ctx
-            .designtime
-            .borrow_mut()
-            .get_orm_mut()
-            .get_node(type_id, temp_node_id)
-            .get_property_definitions()
-            .expect("selected node has properties");
+            self.component_selected.set(true);
 
-        let mut custom_props = vec![];
-        for (value, name, _type_id) in properties {
-            let str_value: String = value
-                .map(|v| match v {
-                    ValueDefinition::LiteralValue(Token { raw_value, .. })
-                    | ValueDefinition::Expression(Token { raw_value, .. }, _)
-                    | ValueDefinition::Identifier(Token { raw_value, .. }, _) => raw_value,
-                    _ => "ERROR: UNSUPPORTED BINDING TYPE".to_owned(),
-                })
-                .unwrap_or("".to_string());
-            match name.as_str() {
-                "x" => self.pos_x.set(StringBox::from(str_value)),
-                "y" => self.pos_y.set(StringBox::from(str_value)),
-                "width" => self.size_width.set(StringBox::from(str_value)),
-                "height" => self.size_height.set(StringBox::from(str_value)),
-                "rotate" => self.rotation_z.set(StringBox::from(str_value)),
-                "scale_x" => self.scale_x.set(StringBox::from(str_value)),
-                "scale_y" => self.scale_y.set(StringBox::from(str_value)),
-                "anchor_x" => self.anchor_x.set(StringBox::from(str_value)),
-                "anchor_y" => self.anchor_y.set(StringBox::from(str_value)),
-                "skew_x" => self.skew_x.set(StringBox::from(str_value)),
-                "skew_y" => self.skew_y.set(StringBox::from(str_value)),
-                custom => custom_props.push(PropertyDef {
-                    name: StringBox::from(custom),
-                    definition: StringBox::from(str_value),
-                }),
+            self.stid.set(StringBox::from(type_id));
+            self.snid.set(temp_node_id.into());
+
+            let (properties, type_name) = ctx
+                .designtime
+                .borrow_mut()
+                .get_orm_mut()
+                .get_node(type_id, temp_node_id)
+                .get_property_definitions()
+                .expect("selected node has properties");
+
+            let mut custom_props = vec![];
+            for (value, name, _type_id) in properties {
+                let str_value: String = value
+                    .map(|v| match v {
+                        ValueDefinition::LiteralValue(Token { raw_value, .. })
+                        | ValueDefinition::Expression(Token { raw_value, .. }, _)
+                        | ValueDefinition::Identifier(Token { raw_value, .. }, _) => raw_value,
+                        _ => "ERROR: UNSUPPORTED BINDING TYPE".to_owned(),
+                    })
+                    .unwrap_or("".to_string());
+                match name.as_str() {
+                    "x" => self.pos_x.set(StringBox::from(str_value)),
+                    "y" => self.pos_y.set(StringBox::from(str_value)),
+                    "width" => self.size_width.set(StringBox::from(str_value)),
+                    "height" => self.size_height.set(StringBox::from(str_value)),
+                    "rotate" => self.rotation_z.set(StringBox::from(str_value)),
+                    "scale_x" => self.scale_x.set(StringBox::from(str_value)),
+                    "scale_y" => self.scale_y.set(StringBox::from(str_value)),
+                    "anchor_x" => self.anchor_x.set(StringBox::from(str_value)),
+                    "anchor_y" => self.anchor_y.set(StringBox::from(str_value)),
+                    "skew_x" => self.skew_x.set(StringBox::from(str_value)),
+                    "skew_y" => self.skew_y.set(StringBox::from(str_value)),
+                    custom => custom_props.push(PropertyDef {
+                        name: StringBox::from(custom),
+                        definition: StringBox::from(str_value),
+                    }),
+                }
             }
-        }
-        self.custom_props.set(custom_props);
-        let (_, name) = type_name.rsplit_once("::").unwrap_or(("", &type_name));
-        self.selected_component_name.set(name.to_owned());
+            self.custom_props.set(custom_props);
+            let (_, name) = type_name.rsplit_once("::").unwrap_or(("", &type_name));
+            self.selected_component_name.set(name.to_owned());
+        });
     }
 }
