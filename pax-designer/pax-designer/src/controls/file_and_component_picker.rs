5673baee157bc395b5f786adb01bf8f53a04049a
Samuel Selleck <samuel@pax.dev>
2024-02-23 12:43:00 -0800

    cleanup

--- pax-designer/pax-designer/src/controls/file_and_component_picker.rs
+++ pax-designer/pax-designer/src/controls/file_and_component_picker.rs
@@ -1,8 +1,8 @@
 use pax_engine::api::*;
 use pax_engine::*;
 
-use pax_std::primitives::Text;
 use pax_std::components::Stacker;
+use pax_std::primitives::Text;
 use pax_std::types::StackerDirection;
 
 #[pax]
