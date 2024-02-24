30135b114584417c2f316def0e762aae33cd57f6
Warfa Jibril <warfaj@gmail.com>
2024-02-23 21:32:02 -0800

    fireworks working

--- pax-runtime/src/rendering.rs
+++ pax-runtime/src/rendering.rs
@@ -7,6 +7,7 @@ use std::rc::Rc;
 
 use crate::api::{CommonProperties, RenderContext, Window};
 use crate::math::Transform2;
+use pax_manifest::UniqueTemplateNodeIdentifier;
 use crate::node_interface::NodeLocal;
 use piet::{Color, StrokeStyle};
 
@@ -30,8 +31,7 @@ pub struct InstantiationArgs {
     ///and recurse into descendant property computation
     pub compute_properties_fn: Option<Box<dyn Fn(&ExpandedNode, &ExpressionTable, &Globals)>>,
 
-    pub template_node_id: usize,
-    pub component_type_id: String,
+    pub template_node_identifier: Option<UniqueTemplateNodeIdentifier>,
 }
 
 /// Stores the computed transform and the pre-transform bounding box (where the
@@ -245,8 +245,7 @@ pub struct BaseInstance {
     pub instance_prototypical_properties_factory: Box<dyn Fn() -> Rc<RefCell<dyn Any>>>,
     pub instance_prototypical_common_properties_factory:
         Box<dyn Fn() -> Rc<RefCell<CommonProperties>>>,
-    pub template_node_id: usize,
-    pub component_type_id: String,
+    pub template_node_identifier: Option<UniqueTemplateNodeIdentifier>,
     instance_children: InstanceNodePtrList,
     flags: InstanceFlags,
 }
@@ -278,8 +277,7 @@ impl BaseInstance {
             instance_prototypical_properties_factory: args.prototypical_properties_factory,
             instance_children: args.children.unwrap_or_default(),
             flags,
-            template_node_id: args.template_node_id,
-            component_type_id: args.component_type_id,
+            template_node_identifier: args.template_node_identifier,
         }
     }
 
