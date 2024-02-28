26c015aacec04b31850e70580d8351572613e436
Zack Brown <zack@pax.dev>
2024-02-27 21:27:54 -0800

    tweak fireworks

--- examples/src/fireworks/src/lib.rs
+++ examples/src/fireworks/src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use pax_engine::api::{Click, ContextMenu, EasingCurve, Event, NodeContext, Wheel};
+use pax_engine::api::*;
 use pax_engine::*;
 use pax_std::primitives::{Ellipse, Frame, Group, Path, Rectangle, Text};
 
@@ -14,13 +14,13 @@ pub struct Fireworks {
 const ROTATION_COEFFICIENT: f64 = 0.00010;
 
 impl Fireworks {
-    pub fn handle_scroll(&mut self, _ctx: &NodeContext, args: Event<Wheel>) {
+    pub fn handle_wheel(&mut self, _ctx: &NodeContext, args: Event<Wheel>) {
         let old_t = self.rotation.get();
         let new_t = old_t - args.delta_y * ROTATION_COEFFICIENT;
         self.rotation.set(f64::max(0.0, new_t));
     }
 
-    pub fn handle_pre_render(&mut self, _ctx: &NodeContext) {
+    pub fn handle_tick(&mut self, _ctx: &NodeContext) {
         let old_ticks = self.ticks.get();
         self.ticks.set(old_ticks + 1);
     }
