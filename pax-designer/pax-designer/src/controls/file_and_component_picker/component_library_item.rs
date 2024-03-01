cb47af9b1c8a64cc24caa0db50d87243ca8030bf
Samuel Selleck <samuel@pax.dev>
2024-02-29 16:20:46 -0800

    component library done

--- pax-designer/pax-designer/src/controls/file_and_component_picker/component_library_item.rs
+++ pax-designer/pax-designer/src/controls/file_and_component_picker/component_library_item.rs
@@ -1,7 +1,20 @@
+use std::ops::ControlFlow;
+
+use model::action::orm::CreateComponent;
 use pax_engine::api::*;
+use pax_engine::math::Vector2;
 use pax_engine::*;
 use pax_std::primitives::Rectangle;
 use pax_std::primitives::Text;
+
+use crate::math::AxisAlignedBox;
+use crate::model;
+use crate::model::action::ActionContext;
+use crate::model::input::Dir;
+use crate::model::input::InputEvent;
+use crate::model::math::coordinate_spaces::Glass;
+use crate::model::ToolBehaviour;
+use math::Point2;
 
 #[pax]
 #[file("controls/file_and_component_picker/component_library_item.pax")]
@@ -14,15 +27,56 @@ pub struct ComponentLibraryItemData {
     pub name: StringBox,
     pub file_path: StringBox,
     pub type_id: String,
+    pub bounds_pixels: (f64, f64),
+}
+
+struct DropComponent {
+    type_id: String,
+    bounds_pixels: (f64, f64),
+}
+
+impl ToolBehaviour for DropComponent {
+    fn pointer_down(&mut self, _point: Point2<Glass>, _ctx: &mut ActionContext) -> ControlFlow<()> {
+        ControlFlow::Continue(())
+    }
+
+    fn pointer_move(&mut self, _point: Point2<Glass>, _ctx: &mut ActionContext) -> ControlFlow<()> {
+        ControlFlow::Continue(())
+    }
+
+    fn pointer_up(&mut self, point: Point2<Glass>, ctx: &mut ActionContext) -> ControlFlow<()> {
+        let w_center = ctx.world_transform() * point;
+        let (w, h) = self.bounds_pixels;
+        let v = Vector2::new(w, h) / 2.0;
+        let bounds = AxisAlignedBox::new(w_center + v, w_center - v);
+        ctx.execute(CreateComponent {
+            bounds,
+            type_id: self.type_id.clone(),
+        })
+        .unwrap();
+        ControlFlow::Break(())
+    }
+
+    fn keyboard(
+        &mut self,
+        _event: InputEvent,
+        _dir: Dir,
+        _ctx: &mut ActionContext,
+    ) -> ControlFlow<()> {
+        ControlFlow::Continue(())
+    }
+
+    fn visualize(&self, _glass: &mut crate::glass::Glass) {}
 }
 
 impl ComponentLibraryItem {
-    pub fn on_click(&mut self, _ctx: &NodeContext, _args: ArgsClick) {
-        pax_engine::log::info!("arm tool to create: {:?}", self.data.get().type_id);
+    pub fn on_down(&mut self, ctx: &NodeContext, _args: ArgsMouseDown) {
+        model::with_action_context(ctx, |ctx| {
+            let data = self.data.get();
+            *ctx.app_state.tool_behaviour.borrow_mut() = Some(Box::new(DropComponent {
+                type_id: data.type_id.clone(),
+                bounds_pixels: data.bounds_pixels,
+            }));
+        });
     }
 }
-
-// TODO:
-// - make button to hide/show library
-// - change click to mousedown -> activates a new ToolBehaviour that drops in the component on pointer up
-// - styling
--- pax-designer/pax-designer/src/controls/file_and_component_picker/mod.rs
+++ pax-designer/pax-designer/src/controls/file_and_component_picker/mod.rs
@@ -10,30 +10,68 @@ use component_library_item::ComponentLibraryItem;
 
 use component_library_item::ComponentLibraryItemData;
 
+use pax_std::primitives::Image;
+use pax_std::primitives::Rectangle;
+
+use crate::model;
+
 #[pax]
 #[file("controls/file_and_component_picker/mod.pax")]
 pub struct FileAndComponentPicker {
     pub library_active: Property<bool>,
     pub registered_components: Property<Vec<ComponentLibraryItemData>>,
+    pub library_active_toggle_image: Property<StringBox>,
 }
 
 impl FileAndComponentPicker {
     pub fn on_mount(&mut self, _ctx: &NodeContext) {
-        self.library_active.set(true);
-        //this data will be set from manifest def later on
-        self.registered_components.set(vec![
-            ComponentLibraryItemData {
-                name: StringBox::from("Rectangle".to_owned()),
-                file_path: StringBox::from("pax-std/src/rectangle.rs"),
-                type_id: "pax_designer::pax_reexports::pax_std::primitives::Rectangle".to_owned(),
-            },
-            ComponentLibraryItemData {
-                name: StringBox::from("Userland Ellipse".to_owned()),
-                file_path: StringBox::from("pax-std/src/rectangle.rs"),
-                type_id: "pax_designer::pax_reexports::pax_std::primitives::Ellipse".to_owned(),
-            },
-        ]);
+        self.library_active_toggle_image
+            .set(StringBox::from("assets/icons/chevron-down.png".to_string()));
     }
 
     pub fn pre_render(&mut self, _ctx: &NodeContext) {}
+
+    pub fn library_toggle(&mut self, ctx: &NodeContext, _args: ArgsClick) {
+        self.library_active.set(!self.library_active.get());
+        let curr = self.library_active.get();
+        self.library_active_toggle_image.set(StringBox::from(
+            match curr {
+                true => "assets/icons/x.png",
+                false => "assets/icons/chevron-down.png",
+            }
+            .to_string(),
+        ));
+
+        let dt = ctx.designtime.borrow_mut();
+        let components: Vec<_> = dt
+            .get_orm()
+            .get_components()
+            .values()
+            .filter_map(|comp| {
+                let is_in_inner_project = comp
+                    .type_id
+                    .starts_with("pax_designer::pax_reexports::designer_project::");
+                let has_template = !comp.is_struct_only_component;
+                let mut is_not_current = false;
+                model::read_app_state(|app_state| {
+                    is_not_current = app_state.selected_component_id != comp.type_id
+                });
+                if is_in_inner_project && has_template && is_not_current {
+                    Some(ComponentLibraryItemData {
+                        name: StringBox::from(comp.type_id.rsplit_once("::")?.1.clone()),
+                        file_path: StringBox::from(comp.module_path.to_owned()),
+                        type_id: comp.type_id.clone(),
+                        bounds_pixels: (200.0, 200.0),
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        self.registered_components.set(match curr {
+            true => components,
+            false => vec![],
+        });
+    }
 }
