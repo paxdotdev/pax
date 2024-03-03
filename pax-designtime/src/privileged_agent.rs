a03330a2dcc32f9674bea947cce93da9a3a8b9bd
Warfa Jibril <warfaj@gmail.com>
2024-03-02 18:25:42 -0800

    cleaning

--- pax-designtime/src/privileged_agent.rs
+++ pax-designtime/src/privileged_agent.rs
@@ -1,6 +1,9 @@
 use std::net::SocketAddr;
 
-use crate::{messages::{AgentMessage, ComponentSerializationRequest}, orm::PaxManifestORM};
+use crate::{
+    messages::{AgentMessage, ComponentSerializationRequest},
+    orm::PaxManifestORM,
+};
 use anyhow::{anyhow, Result};
 use ewebsock::{WsEvent, WsMessage};
 use pax_manifest::ComponentDefinition;
@@ -31,19 +34,21 @@ impl PrivilegedAgentConnection {
     }
 
     pub fn handle_recv(&mut self, manager: &mut PaxManifestORM) -> Result<()> {
-        while let Some(event) = self.recver.try_recv(){
+        while let Some(event) = self.recver.try_recv() {
             match event {
                 WsEvent::Message(ws_message) => {
                     if let WsMessage::Binary(msg_bytes) = ws_message {
                         let msg: AgentMessage = rmp_serde::from_slice(&msg_bytes)?;
                         match msg {
                             AgentMessage::UpdateTemplateRequest(resp) => {
-                                manager.replace_template(resp.type_id, resp.new_template).map_err(|e| anyhow!(e))?;
+                                manager
+                                    .replace_template(resp.type_id, resp.new_template)
+                                    .map_err(|e| anyhow!(e))?;
                             }
                             _ => {}
                         }
                     }
-                },
+                }
                 _ => {}
             }
         }
