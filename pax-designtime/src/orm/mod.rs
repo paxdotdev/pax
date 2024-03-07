f0e1618c87f2fe70d37175d42f0783acfb27d64e
Samuel Selleck <samuel@pax.dev>
2024-03-06 16:25:07 -0800

    node removal

--- pax-designtime/src/orm/mod.rs
+++ pax-designtime/src/orm/mod.rs
@@ -90,7 +90,7 @@ impl PaxManifestORM {
         NodeBuilder::new(self, containing_component_type_id, node_type_id)
     }
 
-    pub fn get_node(&mut self, uni: UniqueTemplateNodeIdentifier) -> NodeBuilder {
+    pub fn get_node(&mut self, uni: UniqueTemplateNodeIdentifier) -> Option<NodeBuilder> {
         NodeBuilder::retrieve_node(self, uni)
     }
 
