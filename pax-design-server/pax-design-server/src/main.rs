b4d1bbf77f4e13a67e58a16514d615ad7021190e
Warfa Jibril <warfaj@gmail.com>
2024-03-07 19:06:06 -0800

    llm working

--- pax-design-server/pax-design-server/src/main.rs
+++ pax-design-server/pax-design-server/src/main.rs
@@ -1,9 +1,14 @@
+use dotenv::dotenv;
 use std::env;
 
 use pax_design_server::start_server;
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("Usage: {} <path_to_watch>", args[0]);
