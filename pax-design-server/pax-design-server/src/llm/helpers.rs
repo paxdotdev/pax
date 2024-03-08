a0efaf614e065fcee7261b4da762e7d89df52cba
Warfa Jibril <warfaj@gmail.com>
2024-03-07 18:45:11 -0800

    llm working

--- pax-design-server/pax-design-server/src/llm/helpers.rs
+++ pax-design-server/pax-design-server/src/llm/helpers.rs
@@ -1,39 +1,42 @@
-use std::fmt::{Display, Formatter};
+use std::{
+    collections::HashMap,
+    fmt::{Display, Formatter},
+};
 
-use pax_designtime::orm::template::{AddTemplateNodeRequest, NodeAction};
-use pax_manifest::{ComponentTemplate, NodeLocation, NodeType, SettingElement, TemplateNodeDefinition, TemplateNodeId, Token, TreeIndexPosition, TreeLocation, TypeId, ValueDefinition};
-use serde::{Deserialize, Serialize};
+use pax_designtime::orm::template::{
+    AddTemplateNodeRequest, NodeAction, RemoveTemplateNodeRequest, UpdateTemplateNodeRequest,
+};
+use pax_manifest::{
+    ComponentTemplate, NodeLocation, NodeType, SettingElement, TemplateNodeDefinition,
+    TemplateNodeId, Token, TreeIndexPosition, TreeLocation, TypeId, UniqueTemplateNodeIdentifier,
+    ValueDefinition,
+};
 
-use crate::llm::SimpleNodeType;
+use super::simple::{
+    SimpleColor, SimpleLocation, SimpleNodeAction, SimpleNodeInformation, SimpleNodeType, SimpleProperties, SimpleRotation, SimpleSize, SimpleSizeType, SimpleTemplate
+};
 
-use super::{SimpleAddNode, SimpleColor, SimpleLocation, SimpleMoveNode, SimpleNodeInformation, SimpleProperties, SimpleRemoveNode, SimpleSize, SimpleTemplate, SimpleUpdateNode, SizeType};
+use super::constants::PREFIX;
 
-pub fn convert_to_simple_node_info(id: &TemplateNodeId, node: &TemplateNodeDefinition) -> SimpleNodeInformation {
-   let node_type = if let Some(p_i) = node.type_id.get_pascal_identifier() {
+pub fn convert_to_simple_node_info(
+    id: &TemplateNodeId,
+    node: &TemplateNodeDefinition,
+) -> SimpleNodeInformation {
+    let node_type = if let Some(p_i) = node.type_id.get_pascal_identifier() {
         match p_i.as_str() {
-            "Rectangle" => {
-                SimpleNodeType::Rectangle
-            },
-            "Ellipse" => {
-                SimpleNodeType::Ellipse
-            },
-            "Text" => {
-                SimpleNodeType::Text
-            },
-            "Group" => {
-                SimpleNodeType::Group
-            },
-            _ => {
-                SimpleNodeType::Other
-            },
+            "Rectangle" => SimpleNodeType::Rectangle,
+            "Ellipse" => SimpleNodeType::Ellipse,
+            "Text" => SimpleNodeType::Text,
+            "Group" => SimpleNodeType::Group,
+            _ => SimpleNodeType::Other,
         }
-   } else {
-         SimpleNodeType::Other
-   };
+    } else {
+        SimpleNodeType::Other
+    };
 
     SimpleNodeInformation {
-         id: id.as_usize(),
-         node_type,
+        id: id.as_usize(),
+        node_type,
     }
 }
 
@@ -64,38 +67,35 @@ impl From<ComponentTemplate> for SimpleTemplate {
     }
 }
 
-
-
 pub fn simple_node_type_to_type_id(node_type: SimpleNodeType) -> Option<TypeId> {
-    const PREFIX : &str= "pax_designer::pax_reexports::pax_std::primitives";
     let t = match node_type {
         SimpleNodeType::Rectangle => {
-            TypeId::build_singleton(&format!("{}::Rectangle", PREFIX) , None)
-        },
-        SimpleNodeType::Ellipse => {
-            TypeId::build_singleton(&format!("{}::Ellipse", PREFIX) , None)
-        },
-        SimpleNodeType::Text => {
-            TypeId::build_singleton(&format!("{}::Text", PREFIX) , None)
-        },
-        SimpleNodeType::Group => {
-            TypeId::build_singleton(&format!("{}::Group", PREFIX) , None)
-        },
+            TypeId::build_singleton(&format!("{}::Rectangle", PREFIX), None)
+        }
+        SimpleNodeType::Ellipse => TypeId::build_singleton(&format!("{}::Ellipse", PREFIX), None),
+        SimpleNodeType::Text => TypeId::build_singleton(&format!("{}::Text", PREFIX), None),
+        SimpleNodeType::Group => TypeId::build_singleton(&format!("{}::Group", PREFIX), None),
         SimpleNodeType::Other => {
             return None;
-        },
+        }
     };
     Some(t)
 }
 
-
 impl Display for SimpleSize {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self.size_type {
-            SizeType::Pixel => write!(f, "{}px", self.value),
-            SizeType::Percent => write!(f, "{}%", self.value),
+            SimpleSizeType::Pixel => write!(f, "{}px", self.value),
+            SimpleSizeType::Percent => write!(f, "{}%", self.value),
         }
     }
+}
+
+impl Display for SimpleRotation {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}deg", self.degrees)
+    }
+
 }
 
 impl Display for SimpleColor {
@@ -106,40 +106,99 @@ impl Display for SimpleColor {
 
 impl From<SimpleSize> for ValueDefinition {
     fn from(value: SimpleSize) -> Self {
-        ValueDefinition::LiteralValue(Token::new_only_raw(value.to_string(), pax_manifest::TokenType::LiteralValue))
+        ValueDefinition::LiteralValue(Token::new_only_raw(
+            value.to_string(),
+            pax_manifest::TokenType::LiteralValue,
+        ))
+    }
+}
+
+impl From<SimpleRotation> for ValueDefinition {
+    fn from(value: SimpleRotation) -> Self {
+        ValueDefinition::LiteralValue(Token::new_only_raw(
+            value.to_string(),
+            pax_manifest::TokenType::LiteralValue,
+        ))
     }
 }
 
 impl From<SimpleColor> for ValueDefinition {
     fn from(value: SimpleColor) -> Self {
-        ValueDefinition::LiteralValue(Token::new_only_raw(value.to_string(), pax_manifest::TokenType::LiteralValue))
+        ValueDefinition::LiteralValue(Token::new_only_raw(
+            value.to_string(),
+            pax_manifest::TokenType::LiteralValue,
+        ))
     }
 }
 
 impl From<SimpleProperties> for NodeType {
     fn from(value: SimpleProperties) -> Self {
-        let mut settings: Vec<SettingElement> = vec![
-        ];
+        let settings: HashMap<Token, Option<ValueDefinition>> = value.into();
+        let template_settings = settings
+            .into_iter()
+            .filter_map(|(k, v)| {
+                if let Some(v) = v {
+                    Some(SettingElement::Setting(k, v))
+                } else {
+                    None
+                }
+            })
+            .collect();
+        NodeType::Template(template_settings)
+    }
+}
+
+impl From<SimpleProperties> for HashMap<Token, Option<ValueDefinition>> {
+    fn from(value: SimpleProperties) -> Self {
+        let mut settings: HashMap<Token, Option<ValueDefinition>> = HashMap::new();
         if let Some(x) = value.x {
-            settings.push(SettingElement::Setting(Token::new_only_raw("x".to_string(), pax_manifest::TokenType::SettingKey), x.into()));
+            settings.insert(
+                Token::new_only_raw("x".to_string(), pax_manifest::TokenType::SettingKey),
+                Some(x.into()),
+            );
         }
         if let Some(y) = value.y {
-            settings.push(SettingElement::Setting(Token::new_only_raw("y".to_string(), pax_manifest::TokenType::SettingKey), y.into()));
+            settings.insert(
+                Token::new_only_raw("y".to_string(), pax_manifest::TokenType::SettingKey),
+                Some(y.into()),
+            );
         }
         if let Some(width) = value.width {
-            settings.push(SettingElement::Setting(Token::new_only_raw("width".to_string(), pax_manifest::TokenType::SettingKey), width.into()));
+            settings.insert(
+                Token::new_only_raw("width".to_string(), pax_manifest::TokenType::SettingKey),
+                Some(width.into()),
+            );
         }
         if let Some(height) = value.height {
-            settings.push(SettingElement::Setting(Token::new_only_raw("height".to_string(), pax_manifest::TokenType::SettingKey), height.into()));
+            settings.insert(
+                Token::new_only_raw("height".to_string(), pax_manifest::TokenType::SettingKey),
+                Some(height.into()),
+            );
         }
+
+        if let Some(rotate) = value.rotate {
+            settings.insert(
+                Token::new_only_raw("rotate".to_string(), pax_manifest::TokenType::SettingKey),
+                Some(rotate.into()),
+            );
+        }
+
         if let Some(fill) = value.fill {
-            //settings.push(SettingElement::Setting(Token::new_only_raw("fill".to_string(), pax_manifest::TokenType::SettingKey), fill.into()));
+            // once color is supported
+            //settings.insert(Token::new_only_raw("fill".to_string(), pax_manifest::TokenType::SettingKey), fill.into());
         }
         if let Some(text) = value.text {
-            settings.push(SettingElement::Setting(Token::new_only_raw("text".to_string(), pax_manifest::TokenType::SettingKey),
-                ValueDefinition::LiteralValue(Token::new_only_raw(text, pax_manifest::TokenType::LiteralValue))));
+            // Pest grammar expects a literal value to be an escaped string
+            let escaped_text = format!("\"{}\"", text);
+            settings.insert(
+                Token::new_only_raw("text".to_string(), pax_manifest::TokenType::SettingKey),
+                Some(ValueDefinition::LiteralValue(Token::new_only_raw(
+                    escaped_text,
+                    pax_manifest::TokenType::LiteralValue,
+                ))),
+            );
         }
-        NodeType::Template(settings)
+        settings
     }
 }
 
@@ -150,44 +209,68 @@ impl From<SimpleLocation> for TreeLocation {
             SimpleLocation::Parent(parent) => TreeLocation::Parent(TemplateNodeId::build(parent)),
         }
     }
-
-}
-
-
-pub enum SimpleNodeAction {
-    Add(SimpleAddNode),
-    Update(SimpleUpdateNode),
-    Remove(SimpleRemoveNode),
-    Move(SimpleMoveNode),
 }
 
 impl SimpleNodeAction {
-    pub fn build(containing_component_type_id: TypeId, action: SimpleNodeAction) -> Option<NodeAction> {
+    pub fn build(
+        containing_component_type_id: TypeId,
+        action: SimpleNodeAction,
+    ) -> Option<NodeAction> {
         match action {
             SimpleNodeAction::Add(add) => {
-                let type_id = simple_node_type_to_type_id(add.node.node_type)?;
-                let node_data = if let Some(properties) = add.node.properties {
+                let type_id = simple_node_type_to_type_id(add.node_type)?;
+                let node_data = if let Some(properties) = add.properties {
                     properties.into()
                 } else {
                     NodeType::Template(vec![])
                 };
-                let tree_location : TreeLocation = add.parent_id.into();
-                let node_location = NodeLocation::new(containing_component_type_id.clone(), tree_location, TreeIndexPosition::Top);
+                let tree_location: TreeLocation = add.parent_id.into();
+                let node_location = NodeLocation::new(
+                    containing_component_type_id.clone(),
+                    tree_location,
+                    TreeIndexPosition::Top,
+                );
 
-                let add = AddTemplateNodeRequest::new(containing_component_type_id, type_id, node_data, Some(node_location));
+                let add = AddTemplateNodeRequest::new(
+                    containing_component_type_id,
+                    type_id,
+                    node_data,
+                    Some(node_location),
+                );
                 Some(NodeAction::Add(add))
-            },
-            SimpleNodeAction::Update(_) => {
-               None
-            },
-            SimpleNodeAction::Remove(_) => {
-                None
-            },
-            SimpleNodeAction::Move(_) => {
-                None
-            },
+            }
+            SimpleNodeAction::Update(update) => {
+                let template_node_id = TemplateNodeId::build(update.id);
+                let uni = UniqueTemplateNodeIdentifier::build(
+                    containing_component_type_id.clone(),
+                    template_node_id,
+                );
+                let mut updated_property_map = HashMap::new();
+                if let Some(properties) = update.properties {
+                    updated_property_map = properties.into();
+                }
+                let new_location = if let Some(new_parent) = update.new_parent {
+                    Some(NodeLocation::new(
+                        containing_component_type_id.clone(),
+                        new_parent.into(),
+                        TreeIndexPosition::Top,
+                    ))
+                } else {
+                    None
+                };
+                let update =
+                    UpdateTemplateNodeRequest::new(uni, updated_property_map, new_location);
+                Some(NodeAction::Update(update))
+            }
+            SimpleNodeAction::Remove(remove) => {
+                let template_node_id = TemplateNodeId::build(remove.id);
+                let uni = UniqueTemplateNodeIdentifier::build(
+                    containing_component_type_id,
+                    template_node_id,
+                );
+                let remove = RemoveTemplateNodeRequest::new(uni);
+                Some(NodeAction::Remove(remove))
+            }
         }
     }
 }
-
-
---
+++ pax-design-server/pax-design-server/src/llm/simple.rs
@@ -0,0 +1,125 @@
+use std::collections::HashMap;
+
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+/// All possible actions that the LLM can perform
+#[derive(Debug)]
+pub enum SimpleNodeAction {
+    Add(SimpleAdd),
+    Update(SimpleUpdate),
+    Remove(SimpleRemove),
+}
+
+/// A node with the specified properties will be added to the scene at the specified location.
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct SimpleAdd {
+    pub parent_id: SimpleLocation,
+    pub node_type: SimpleNodeType,
+    pub properties: Option<SimpleProperties>,
+}
+
+/// The node specified by id will be updated with the new properties or new parent
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct SimpleUpdate {
+    pub id: usize,
+    pub properties: Option<SimpleProperties>,
+    pub new_parent: Option<SimpleLocation>,
+}
+
+/// The node specified by id will be removed from the scene
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct SimpleRemove {
+    pub id: usize,
+}
+
+// Information about the outer viewport
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ViewportInformation {
+    pub height: SimpleSize,
+    pub width: SimpleSize,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SimpleTemplate {
+    pub root_nodes: Vec<SimpleNodeInformation>,
+    pub children: HashMap<usize, Vec<SimpleNodeInformation>>,
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SimpleNodeInformation {
+    pub id: usize,
+    pub node_type: SimpleNodeType,
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct SimpleWorldInformation {
+    pub viewport: ViewportInformation,
+    pub template: SimpleTemplate,
+}
+
+
+/// The only possible node types that can be added to the scene. Only Group and Other can have children
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub enum SimpleNodeType {
+    Rectangle,
+    Ellipse,
+    Text,
+    Group,
+    Other,
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct SimpleNode {
+    pub id: usize,
+    pub node_type: SimpleNodeType,
+    pub properties: Option<SimpleProperties>,
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct SimpleColor {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct SimpleProperties {
+    pub x: Option<SimpleSize>,
+    pub y: Option<SimpleSize>,
+    pub width: Option<SimpleSize>,
+    pub height: Option<SimpleSize>,
+    pub fill: Option<SimpleColor>,
+    pub rotate: Option<SimpleRotation>,
+    pub text: Option<String>,
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub enum SimpleLocation {
+    Root,
+    Parent(usize),
+}
+
+
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub enum SimpleSizeType {
+    Pixel,
+    Percent,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct SimpleSize {
+    pub value: f32,
+    pub size_type: SimpleSizeType,
+}
+
+/// The rotation of a node in degrees
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct SimpleRotation {
+    pub degrees: f32,
+}
