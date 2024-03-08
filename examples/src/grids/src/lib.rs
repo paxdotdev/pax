e9729a08ea7e498f6af26e6b40e3a3c17a032bfe
Zack Brown <zack@pax.dev>
2024-03-07 16:11:58 -0800

    wip: update example grids

--- examples/src/grids/src/lib.rs
+++ examples/src/grids/src/lib.rs
@@ -1,8 +1,8 @@
 use pax_engine::api::*;
 use pax_engine::*;
-use pax_std::primitives::{Ellipse, Frame, Group, Path, Rectangle, Text};
+use pax_std::primitives::*;
 use pax_std::components::{Stacker};
-use pax_std::types::{Color, StackerDirection};
+use pax_std::types::{StackerDirection};
 
 
 #[pax]
