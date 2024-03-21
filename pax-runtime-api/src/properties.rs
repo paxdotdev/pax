43269dc222e1c3053b65c1ef22ff6fe51e522945
Samuel Selleck <samuel@pax.dev>
2024-03-20 17:01:41 -0700

    removed subscribe, recursive props working

--- pax-runtime-api/src/properties.rs
+++ pax-runtime-api/src/properties.rs
@@ -3,40 +3,74 @@ use std::{any::Any, cell::RefCell, marker::PhantomData, rc::Rc};
 use serde::{Deserialize, Serialize};
 use slotmap::SlotMap;
 
-use self::private::PropId;
+use self::private::{HasPropId, PropId};
 
-#[derive(Clone, Copy, Debug)]
+/// Reactive property type. Shallow clones can be cheaply made.
+#[derive(Debug)]
 pub struct Property<T> {
     id: PropId,
     _phantom: PhantomData<T>,
 }
 
-impl<'de, T: Default + PropVal> Default for Property<T> {
+impl<T> Clone for Property<T> {
+    fn clone(&self) -> Self {
+        glob_prop_table(|t| {
+            t.with_prop_data_mut(self.id, |prop_data| {
+                prop_data.ref_count += 1;
+            });
+        });
+        Property {
+            id: self.id,
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> Drop for Property<T> {
+    fn drop(&mut self) {
+        glob_prop_table(|t| {
+            let ref_count = t.with_prop_data_mut(self.id, |prop_data| {
+                prop_data.ref_count -= 1;
+                prop_data.ref_count
+            });
+            if ref_count == 0 {
+                t.remove_entry(self.id);
+            }
+        });
+    }
+}
+impl<T: Default + PropVal> Default for Property<T> {
     fn default() -> Self {
         Property::new(T::default())
     }
 }
 
+/// PropVal represents a restriction on valid generic types that a property can
+/// contain. All T need to be Clone (to enable .get()) + 'static (no references/
+/// lifetimes)
 pub trait PropVal: Clone + 'static {}
-impl<'de, T: Clone + 'static> PropVal for T {}
+impl<T: Clone + 'static> PropVal for T {}
 
-// Don't expose these outside this module
 mod private {
     slotmap::new_key_type!(
+        /// Sealed PropId (should be internal impl detail)
         pub struct PropId;
     );
 
+    /// Used to restrict dependencies to properties
     pub trait HasPropId {
         fn get_id(&self) -> PropId;
     }
 }
 
-impl<T> private::HasPropId for Property<T> {
+impl<T> HasPropId for Property<T> {
     fn get_id(&self) -> PropId {
         self.id
     }
 }
 
+/// Note that serialization and deserialization fully disconnects properties,
+/// and only loads them back in as literals.
 impl<'de, T: PropVal + Deserialize<'de>> Deserialize<'de> for Property<T> {
     fn deserialize<D>(deserializer: D) -> Result<Property<T>, D::Error>
     where
@@ -46,12 +80,12 @@ impl<'de, T: PropVal + Deserialize<'de>> Deserialize<'de> for Property<T> {
         Ok(Property::new(value))
     }
 }
-
 impl<T: PropVal + Serialize> Serialize for Property<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
+        // TODO check if literal or expression, error on expression?
         self.get().serialize(serializer)
     }
 }
@@ -69,65 +103,33 @@ impl<T: PropVal> Property<T> {
         }
     }
 
-    pub fn expression(
-        evaluator: impl Fn() -> Box<dyn Any> + 'static,
-        dependents: &[&dyn private::HasPropId],
-    ) -> Self {
+    /// Used by engine to create dependency chains, the evaluator fires and
+    /// re-computes a property each time it's dependencies change.
+    pub fn expression(evaluator: impl Fn() -> T + 'static, dependents: &[&dyn HasPropId]) -> Self {
         let dependent_property_ids: Vec<_> = dependents.iter().map(|v| v.get_id()).collect();
-        let id = glob_prop_table(|t| t.add_expr_entry(Rc::new(evaluator), &dependent_property_ids));
+        let id = glob_prop_table(|t| {
+            t.add_expr_entry(
+                Rc::new(move || Box::new(evaluator())),
+                &dependent_property_ids,
+            )
+        });
         Self {
             id,
             _phantom: PhantomData {},
         }
     }
 
-    pub fn subscribe(&self, f: impl Fn() + 'static) {
-        glob_prop_table(|t| {
-            t.with_prop_data(self.id, |prop_data| {
-                prop_data.on_change.push(Rc::new(f));
-            })
-        })
-    }
-
+    /// Gets the currently stored value. Might be computationally
+    /// expensive in a large reactivity network since this triggers
+    /// re-evaluation of dirty property chains
     pub fn get(&self) -> T {
         glob_prop_table(|t| t.get_value(self.id))
     }
 
+    /// Sets this properties value, and sets the drity bit of all of
+    /// it's dependencies if not already set
     pub fn set(&self, val: T) {
         glob_prop_table(|t| t.set_value(self.id, val));
-    }
-}
-
-pub struct PropertyScopeHandle {
-    ids: Vec<PropId>,
-}
-
-impl PropertyScopeHandle {
-    pub fn new<V>(f: impl FnOnce() -> V) -> (V, Self) {
-        let before = glob_prop_table(|t| t.trace_creation_start());
-        let res = f();
-        let created_prpoperty_ids =
-            glob_prop_table(|t| t.trace_creation_end(before)).expect("was started");
-        (
-            res,
-            Self {
-                ids: created_prpoperty_ids,
-            },
-        )
-    }
-
-    pub fn drop_all(mut self) {
-        for id in self.ids.drain(0..) {
-            glob_prop_table(|t| t.remove_entry(id));
-        }
-    }
-}
-
-impl Drop for PropertyScopeHandle {
-    fn drop(&mut self) {
-        if !self.ids.is_empty() {
-            panic!("PropertyScopeHandle .drop_all() must be called manually before being dropped to clean up associated properties")
-        }
     }
 }
 
@@ -139,8 +141,8 @@ fn glob_prop_table<V>(f: impl FnOnce(&PropertyTable) -> V) -> V {
 }
 
 pub struct PropertyTable {
-    entires: RefCell<SlotMap<PropId, PropertyData>>,
-    creation_trace: RefCell<Option<Vec<PropId>>>,
+    entries: RefCell<SlotMap<PropId, RefCell<PropertyData>>>,
+    // creation_trace: RefCell<Option<Vec<PropId>>>,
 }
 
 impl std::fmt::Debug for PropertyTable {
@@ -149,10 +151,10 @@ impl std::fmt::Debug for PropertyTable {
             .field(
                 "prop_ids",
                 &self
-                    .entires
+                    .entries
                     .borrow()
                     .iter()
-                    .map(|(k, v)| (k, &v.subscribers))
+                    .map(|(k, v)| (k, v.borrow().subscribers.to_owned()))
                     .collect::<Vec<_>>(),
             )
             .finish_non_exhaustive()
@@ -162,24 +164,19 @@ impl std::fmt::Debug for PropertyTable {
 impl PropertyTable {
     pub fn new() -> Self {
         Self {
-            entires: Default::default(),
-            creation_trace: RefCell::new(None),
+            entries: Default::default(),
+            // creation_trace: RefCell::new(None),
         }
     }
 
     fn add_literal_entry<T: PropVal>(&self, val: T) -> PropId {
-        let mut sm = self.entires.borrow_mut();
-        let prop_id = sm.insert(PropertyData {
+        let mut sm = self.entries.borrow_mut();
+        let prop_id = sm.insert(RefCell::new(PropertyData {
+            ref_count: 1,
             value: Box::new(val),
             subscribers: Vec::with_capacity(0),
-            on_change: Vec::with_capacity(0),
-            prop_type: PropType::Literal(),
-        });
-        let mut trace = self.creation_trace.borrow_mut();
-        match trace.as_mut() {
-            Some(trace) => trace.push(prop_id),
-            None => panic!("can't create a property outside of a PropertyScopeHandle::drop_scope"),
-        }
+            prop_type: PropType::Literal,
+        }));
         prop_id
     }
 
@@ -189,42 +186,56 @@ impl PropertyTable {
         dependents: &[PropId],
     ) -> PropId {
         let expr_id = {
-            let mut sm = self.entires.borrow_mut();
-            sm.insert(PropertyData {
+            let mut sm = self.entries.borrow_mut();
+            sm.insert(RefCell::new(PropertyData {
+                ref_count: 1,
                 value: Box::new(()),
                 subscribers: Vec::with_capacity(0),
-                on_change: Vec::with_capacity(0),
                 prop_type: PropType::Expr {
                     dirty: true,
                     evaluator,
                     subscriptions: dependents.to_vec(),
                 },
-            })
+            }))
         };
-        let mut trace = self.creation_trace.borrow_mut();
-        match trace.as_mut() {
-            Some(trace) => trace.push(expr_id),
-            None => panic!("can't create a property outside of a PropertyScopeHandle::drop_scope"),
-        }
         for id in dependents {
-            self.with_prop_data(*id, |dep_prop| {
+            self.with_prop_data_mut(*id, |dep_prop| {
                 dep_prop.subscribers.push(expr_id);
             });
         }
         expr_id
     }
 
-    fn with_prop_data<V>(&self, id: PropId, f: impl FnOnce(&mut PropertyData) -> V) -> V {
-        let mut sm = self.entires.borrow_mut();
-        let prop_data = sm.get_mut(id).expect(
-            "tried to access property entry that doesn't exist anymore,\
-is the corresponding PropertyScope already removed?",
-        );
-        f(prop_data)
+    fn with_prop_data_mut<V>(&self, id: PropId, f: impl FnOnce(&mut PropertyData) -> V) -> V {
+        let sm = self.entries.borrow();
+        let mut prop_data = sm
+            .get(id)
+            .expect(
+                "tried to access property entry that doesn't exist anymore,\
+ is it's PropertyScope already cleaned up?",
+            )
+            .try_borrow_mut()
+            .expect("tried to access same property internals recursively");
+        let res = f(&mut *prop_data);
+        res
+    }
+
+    fn with_prop_data<V>(&self, id: PropId, f: impl FnOnce(&PropertyData) -> V) -> V {
+        let sm = self.entries.borrow();
+        let prop_data = sm
+            .get(id)
+            .expect(
+                "tried to access property entry that doesn't exist anymore,\
+ is it's PropertyScope already cleaned up?",
+            )
+            .try_borrow()
+            .expect("tried to access same property interals recursively while mutably borrowed");
+        let res = f(&*prop_data);
+        res
     }
 
     fn get_value<T: PropVal>(&self, id: PropId) -> T {
-        let evaluator = self.with_prop_data(id, |prop_data| {
+        let evaluator = self.with_prop_data_mut(id, |prop_data| {
             if let PropType::Expr {
                 dirty, evaluator, ..
             } = &mut prop_data.prop_type
@@ -238,60 +249,53 @@ is the corresponding PropertyScope already removed?",
             }
             None
         });
-
         if let Some(evaluator) = evaluator {
             let new_value = evaluator();
-            self.with_prop_data(id, |prop_data| {
+            self.with_prop_data_mut(id, |prop_data| {
                 prop_data.value = new_value;
             });
         }
 
-        self.with_prop_data(id, |prop_data| {
+        let ret = self.with_prop_data(id, |prop_data| {
             let value = prop_data.value.downcast_ref::<T>().expect("correct type");
             value.clone()
-        })
+        });
+        ret
     }
 
     fn set_value<T: PropVal>(&self, id: PropId, value: T) {
         let mut to_dirty = vec![];
-        let on_change_handlers = self.with_prop_data(id, |prop_data| {
+        self.with_prop_data_mut(id, |prop_data| {
             prop_data.value = Box::new(value);
             to_dirty.extend_from_slice(&prop_data.subscribers);
-            prop_data.on_change.clone()
         });
-        for f in &on_change_handlers {
-            f()
-        }
-
         while let Some(dep_id) = to_dirty.pop() {
-            let on_change_handlers = self.with_prop_data(dep_id, |dep_data| {
+            self.with_prop_data_mut(dep_id, |dep_data| {
                 if dep_id == id {
-                    panic!("property cycle detected");
+                    unreachable!(
+                        "property cycles should never happen with literals/expressions being a DAG"
+                    );
                 }
                 let PropType::Expr { ref mut dirty, .. } = dep_data.prop_type else {
                     unreachable!("non-expressions shouldn't depend on other properties")
                 };
-                let ret = dep_data.on_change.clone();
                 if !*dirty {
+                    *dirty = true;
                     to_dirty.extend_from_slice(&dep_data.subscribers);
                 }
-                *dirty = true;
-                ret
             });
-            for f in &on_change_handlers {
-                f()
-            }
         }
     }
 
     fn remove_entry(&self, id: PropId) {
         let prop_data = {
-            let mut sm = self.entires.borrow_mut();
+            let mut sm = self.entries.borrow_mut();
             let prop_data = sm.remove(id).expect("tried to remove non-existent prop");
             prop_data
-        };
+        }
+        .into_inner();
         for sub in prop_data.subscribers {
-            self.with_prop_data(sub, |s| {
+            self.with_prop_data_mut(sub, |s| {
                 if let PropType::Expr { subscriptions, .. } = &mut s.prop_type {
                     subscriptions.retain(|s| s != &id);
                 }
@@ -299,36 +303,23 @@ is the corresponding PropertyScope already removed?",
         }
         if let PropType::Expr { subscriptions, .. } = prop_data.prop_type {
             for subscription in subscriptions {
-                self.with_prop_data(subscription, |sub| {
+                self.with_prop_data_mut(subscription, |sub| {
                     sub.subscribers.retain(|v| v != &id);
                 });
             }
         }
-    }
-
-    fn trace_creation_start(&self) -> Option<Vec<PropId>> {
-        let mut trace = self.creation_trace.borrow_mut();
-        std::mem::replace(&mut trace, Some(vec![]))
-    }
-
-    fn trace_creation_end(&self, mut old_values: Option<Vec<PropId>>) -> Option<Vec<PropId>> {
-        let mut trace = self.creation_trace.borrow_mut();
-        std::mem::swap(&mut *trace, &mut old_values);
-        old_values
     }
 }
 
 struct PropertyData {
     value: Box<dyn Any>,
     subscribers: Vec<PropId>,
-    on_change: Vec<Rc<dyn Fn()>>,
+    ref_count: usize,
     prop_type: PropType,
 }
 
 enum PropType {
-    Literal(),
-    // expr index (in Vtable), and the id's that
-    // should make this expr update
+    Literal,
     Expr {
         //
         evaluator: Rc<dyn Fn() -> Box<dyn Any>>,
@@ -340,116 +331,69 @@ enum PropType {
 #[cfg(test)]
 mod tests {
 
-    use std::{cell::Cell, rc::Rc};
-
     use super::*;
 
     #[test]
     fn test_literal_set_get() {
-        let (prop, handle) = PropertyScopeHandle::new(|| Property::literal(5));
+        let prop = Property::literal(5);
         assert_eq!(prop.get(), 5);
         prop.set(2);
         assert_eq!(prop.get(), 2);
-        handle.drop_all();
     }
 
     #[test]
     fn test_expression_get() {
-        let (prop, handle) =
-            PropertyScopeHandle::new(|| Property::<i32>::expression(|| Box::new(42), &[]));
+        let prop = Property::<i32>::expression(|| 42, &[]);
         assert_eq!(prop.get(), 42);
-        handle.drop_all();
     }
 
     #[test]
     fn test_expression_dependent_on_literal() {
-        let ((prop_1, prop_2), handle) = PropertyScopeHandle::new(|| {
-            let prop_1 = Property::literal(2);
-            let prop_2 =
-                Property::<i32>::expression(move || Box::new(prop_1.get() * 5), &[&prop_1]);
-            (prop_1, prop_2)
-        });
+        let prop_1 = Property::literal(2);
+        let p1 = prop_1.clone();
+        let prop_2 = Property::<i32>::expression(move || p1.get() * 5, &[&prop_1]);
 
         assert_eq!(prop_2.get(), 10);
         prop_1.set(3);
         assert_eq!(prop_2.get(), 15);
-        handle.drop_all();
-    }
-
-    #[test]
-    fn test_subscribe() {
-        let (prop, handle) = PropertyScopeHandle::new(|| Property::literal(5));
-        let triggered = Rc::new(Cell::new(false));
-        let sub_run = triggered.clone();
-        prop.subscribe(move || {
-            sub_run.set(true);
-        });
-        prop.set(3);
-        handle.drop_all();
-        assert!(triggered.get());
     }
 
     #[test]
     fn test_larger_network() {
-        let ((prop_1, prop_2, prop_3, prop_4), handle) = PropertyScopeHandle::new(|| {
-            let prop_1 = Property::literal(2);
-            let prop_2 = Property::literal(6);
-            let prop_3 = Property::<i32>::expression(
-                move || Box::new(prop_1.get() * prop_2.get()),
-                &[&prop_1, &prop_2],
-            );
-            let prop_4 = Property::<i32>::expression(
-                move || Box::new(prop_1.get() + prop_3.get()),
-                &[&prop_1, &prop_3],
-            );
-            (prop_1, prop_2, prop_3, prop_4)
-        });
+        let prop_1 = Property::literal(2);
+        let prop_2 = Property::literal(6);
 
-        let sub_value = Rc::new(Cell::new(0));
-        let sub_value_cl = sub_value.clone();
-        prop_4.subscribe(move || {
-            if prop_4.get() > 3 {
-                sub_value_cl.set(prop_1.get());
-            } else {
-                sub_value_cl.set(prop_3.get());
-            }
-        });
+        let p1 = prop_1.clone();
+        let p2 = prop_2.clone();
+        let prop_3 = Property::expression(move || p1.get() * p2.get(), &[&prop_1, &prop_2]);
+        let p1 = prop_1.clone();
+        let p3 = prop_3.clone();
+        let prop_4 = Property::expression(move || p1.get() + p3.get(), &[&prop_1, &prop_3]);
+
         assert_eq!(prop_4.get(), 14);
         prop_1.set(1);
         assert_eq!(prop_4.get(), 7);
-        assert_eq!(sub_value.get(), 1);
         prop_2.set(2);
         assert_eq!(prop_4.get(), 3);
-        assert_eq!(sub_value.get(), 2);
-        handle.drop_all();
     }
 
     #[test]
     fn test_cleanup() {
-        assert!(glob_prop_table(|t| t.entires.borrow().is_empty()));
-        let (_, handle) = PropertyScopeHandle::new(|| Property::literal(5));
-        assert_eq!(glob_prop_table(|t| t.entires.borrow().len()), 1);
-        handle.drop_all();
-        assert!(glob_prop_table(|t| t.entires.borrow().is_empty()));
+        assert!(glob_prop_table(|t| t.entries.borrow().is_empty()));
+        let prop = Property::literal(5);
+        assert_eq!(glob_prop_table(|t| t.entries.borrow().len()), 1);
+        drop(prop);
+        assert!(glob_prop_table(|t| t.entries.borrow().is_empty()));
     }
 
     #[test]
-    #[should_panic]
-    fn test_use_property_after_scope_dropped() {
-        let (prop, handle) = PropertyScopeHandle::new(|| Property::literal(5));
-        handle.drop_all();
-        prop.get();
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_no_prop_creation_outside_of_scope() {
-        Property::literal(5);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_scope_handle_not_call_drop_all() {
-        let (_, _handle) = PropertyScopeHandle::new(|| Property::literal(5));
+    fn test_recursive_props() {
+        {
+            let prop_of_prop = Property::literal(Property::literal(3));
+            let prop_of_prop_clone = prop_of_prop.clone();
+            prop_of_prop_clone.get().set(1);
+            assert_eq!(prop_of_prop.get().get(), prop_of_prop_clone.get().get());
+        }
+        assert!(glob_prop_table(|t| t.entries.borrow().is_empty()));
     }
 }
