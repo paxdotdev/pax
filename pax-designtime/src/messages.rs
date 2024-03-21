8ddc64e1f9ec6e3e831ea5a97c00da516e1410b5
Warfa Jibril <warfaj@gmail.com>
2024-03-20 20:16:33 -0700

    fixing so release works

--- pax-designtime/src/messages.rs
+++ pax-designtime/src/messages.rs
@@ -43,7 +43,7 @@ pub struct UpdateTemplateRequest {
 
 /// A request for help from the LLM.
 /// Sent from `pax-designtime` to `pax-design-server`.
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct LLMHelpRequest {
     pub request: String,
     pub component: ComponentDefinition,
