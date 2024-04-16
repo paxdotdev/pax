3de94bd4d810876e6782e9cc0e2851cdd947b79e
Samuel Selleck <samuel@pax.dev>
2024-04-16 16:47:16 -0700

    raw path string, release esbuild js files

--- scripts/release.py
+++ scripts/release.py
@@ -184,7 +184,14 @@ for root in root_packages:
         # Only publish the package if it has not been published in this run
         if elem not in published:
             # Run `cargo publish` within the current package directory
-            subprocess.run(["cargo", "publish", "--no-verify", "--allow-dirty"], cwd=os.path.join(os.getcwd(), elem), check=True)
+            args = ["cargo", "publish", "--no-verify"]
+
+            # we pre-build artifacts (js/css) that are not checked in to commit
+            # publish doesn't like this if we don't include --allow-dirty
+            if elem == "pax-chassis-web":
+                args.push("--allow-dirty")
+
+            subprocess.run(["cargo", "publish", "--no-verify"], cwd=os.path.join(os.getcwd(), elem), check=True)
             # Mark this package as published
             published.add(elem)
             # Wait one minute, to satisfy crates.io's throttling mechanism.
