7e224dbe4f1989ff0419c16ff07a99c041434d2e
Samuel Selleck <samuel@pax.dev>
2024-04-26 15:38:55 -0700

    slot particle example finished

--- examples/src/slot-particles/src/slot_particles/mod.rs
+++ examples/src/slot-particles/src/slot_particles/mod.rs
@@ -14,11 +14,12 @@ use std::rc::Rc;
 #[main]
 #[file("slot_particles/mod.pax")]
 pub struct SlotParticles {
-    pub particles: Property<Vec<ParticleData>>,
-    pub persistent_rng_data: Property<Vec<RngData>>,
+    pub config: Property<Config>,
 
     //private
     pub particle_len: Property<usize>,
+    pub persistent_rng_data: Property<Vec<RngData>>,
+    pub particles: Property<Vec<ParticleData>>,
 }
 
 impl SlotParticles {
@@ -28,27 +29,35 @@ impl SlotParticles {
         let rng = Rc::new(RefCell::new(rand::thread_rng()));
         let bounds = ctx.bounds_self.clone();
         let store = Rc::new(RefCell::new(Vec::new()));
+        let config = self.config.clone();
         self.persistent_rng_data.replace_with(Property::computed(
             move || {
                 let mut store = store.borrow_mut();
+                let config = config.get();
+                let min_size = config.min_size.get().to_float();
+                let max_size = config.max_size.get().to_float();
+                let max_speed = config.max_speed.get().to_float();
+                let max_rotation = config.max_rotation.get().to_float();
                 let mut rng = rng.borrow_mut();
                 let (w, h) = bounds.get();
+                let w = w.max(1.0);
+                let h = h.max(1.0);
                 while store.len() < num.get() {
                     store.push(RngData {
                         x: rng.gen_range(0.0..w),
                         y: rng.gen_range(0.0..h),
-                        dx: rng.gen_range(-1.0..1.0),
-                        dy: rng.gen_range(-1.0..1.0),
-                        r: rng.gen_range(-1.0..1.0),
+                        s: rng.gen_range(min_size..max_size),
+                        dx: rng.gen_range(-max_speed..max_speed),
+                        dy: rng.gen_range(-max_speed..max_speed),
+                        r: rng.gen_range(-max_rotation..max_rotation),
                     });
                 }
                 store.clone()
             },
-            &[self.particle_len.untyped()],
+            &[self.particle_len.untyped(), self.config.untyped()],
         ));
         let bounds = ctx.bounds_self.clone();
         let base_data = self.persistent_rng_data.clone();
-        const SIZE: f64 = 30.0;
         self.particles.replace_with(Property::computed(
             move || {
                 let t = tick.get() as f64;
@@ -56,10 +65,10 @@ impl SlotParticles {
                 let (w, h) = bounds.get();
                 base.iter()
                     .map(|b| ParticleData {
-                        x: (b.x + t * b.dx).rem_euclid(w + 4.0 * SIZE) - 2.0 * SIZE,
-                        y: (b.y + t * b.dy).rem_euclid(h + 4.0 * SIZE) - 2.0 * SIZE,
-                        width: SIZE,
-                        height: SIZE,
+                        x: (b.x + t * b.dx).rem_euclid(w + 4.0 * b.s) - 2.0 * b.s,
+                        y: (b.y + t * b.dy).rem_euclid(h + 4.0 * b.s) - 2.0 * b.s,
+                        width: b.s,
+                        height: b.s,
                         rotate: t * b.r,
                     })
                     .collect()
@@ -80,12 +89,21 @@ impl SlotParticles {
 }
 
 #[pax]
+pub struct Config {
+    pub max_size: Property<Numeric>,
+    pub min_size: Property<Numeric>,
+    pub max_rotation: Property<Numeric>,
+    pub max_speed: Property<Numeric>,
+}
+
+#[pax]
 pub struct RngData {
     pub x: f64,
     pub y: f64,
     pub dx: f64,
     pub dy: f64,
     pub r: f64,
+    pub s: f64,
 }
 
 #[pax]
