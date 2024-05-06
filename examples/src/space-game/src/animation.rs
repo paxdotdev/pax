0c601171a6f76f8fb672ffa93f0c7bc57699089b
Samuel Selleck <samuel@pax.dev>
2024-05-06 10:05:29 -0700

    space game base

---
+++ examples/src/space-game/src/animation.rs
@@ -0,0 +1,50 @@
+
+use pax_engine::api::*;
+use pax_engine::*;
+#[pax]
+pub struct Animation {
+    pub frame: String,
+    pub frames: Vec<String>,
+    pub running: bool,
+    pub finished: bool,
+    pub time: u64,
+
+    //settable properties
+    pub speed: u64,
+    pub loops: bool,
+}
+
+impl Animation {
+    pub fn new(frames: Vec<String>) -> Self {
+        Self {
+            frame: frames[0].clone(),
+            frames,
+            running: false,
+            loops: false,
+            finished: false,
+            speed: 10,
+            time: 0,
+        }
+    }
+
+    pub fn start(&mut self) {
+        self.running = true;
+    }
+
+    pub fn tick(&mut self) {
+        if self.running {
+            self.time += 1;
+            let frame_index = (self.time / self.speed) as usize;
+            if frame_index < self.frames.len() {
+                self.frame = self.frames[frame_index].clone();
+                return;
+            }
+            if self.loops {
+                self.start();
+            } else {
+                self.running = false;
+                self.finished = true;
+            }
+        }
+    }
+}
