bbcebeeda619759b3ba374d0672db0fee5e594fb
Warfa Jibril <warfaj@gmail.com>
2024-05-17 12:26:42 -0700

    fixing intro example

--- pax-compiler/new-project-template/src/lib.rs
+++ pax-compiler/new-project-template/src/lib.rs
@@ -14,13 +14,9 @@ use pax_std::types::*;
 pub struct Example {
     pub ticks: Property<usize>,
     pub num_clicks: Property<usize>,
-    pub message: Property<String>,
 }
 
 impl Example {
-    pub fn handle_mount(&mut self, ctx: &NodeContext) {
-        self.message.set("Click me".to_string());
-    }
     pub fn handle_pre_render(&mut self, ctx: &NodeContext) {
         let old_ticks = self.ticks.get();
         self.ticks.set((old_ticks + 1) % 255);
@@ -29,7 +25,5 @@ impl Example {
     pub fn increment(&mut self, ctx: &NodeContext, args: Event<Click>) {
         let old_num_clicks = self.num_clicks.get();
         self.num_clicks.set(old_num_clicks + 1);
-        self.message
-            .set(format!("{} clicks", self.num_clicks.get()));
     }
 }
