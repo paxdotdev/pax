450a4a188e9443e1e96ced896b6370c751913c70
Samuel Selleck <samuel@pax.dev>
2024-05-21 14:36:31 -0700

    resizable start

---
+++ examples/src/ui-components/src/lib.rs
@@ -0,0 +1,22 @@
+#![allow(unused_imports)]
+
+pub mod components;
+pub use components::resizable::Resizable;
+
+use pax_engine::api::*;
+use pax_engine::*;
+use pax_std::components::Stacker;
+use pax_std::components::*;
+use pax_std::primitives::*;
+use pax_std::types::text::*;
+use pax_std::types::*;
+
+#[pax]
+#[main]
+#[file("lib.pax")]
+pub struct Example {
+    pub ticks: Property<usize>,
+    pub num_clicks: Property<usize>,
+}
+
+impl Example {}
