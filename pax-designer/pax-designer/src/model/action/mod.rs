756ec3d60d5b747fbe25d5adacfa02a2402c10bc
Samuel Selleck <samuel@pax.dev>
2024-06-10 19:34:01 -0700

    rotation in new sys

--- pax-designer/pax-designer/src/model/action/mod.rs
+++ pax-designer/pax-designer/src/model/action/mod.rs
@@ -14,6 +14,7 @@ use pax_engine::{
 };
 use pax_engine::{log, Property};
 use pax_manifest::{TemplateNodeId, UniqueTemplateNodeIdentifier};
+use pax_runtime_api::{Axis, Size};
 
 use crate::math::coordinate_spaces::Glass;
 
@@ -163,8 +164,34 @@ impl ActionContext<'_> {
                             &deps,
                         )
                     },
-                    props: n.layout_properties(),
-                    origin: to_glass_transform.get() * n.origin()?,
+                    origin: {
+                        let parent_t_and_b = n
+                            .parent()
+                            .map(|p| p.transform_and_bounds())
+                            .unwrap_or_default();
+                        let properties = n.layout_properties();
+                        let deps = [parent_t_and_b.untyped(), to_glass_transform.untyped()];
+                        let to_glass = to_glass_transform.clone();
+                        Property::computed(
+                            move || {
+                                let parent_t_and_b = parent_t_and_b.get();
+                                let parent_bounds = parent_t_and_b.bounds;
+                                let local_x = properties
+                                    .x
+                                    .unwrap_or(Size::ZERO())
+                                    .evaluate(parent_bounds, Axis::X);
+                                let local_y = properties
+                                    .y
+                                    .unwrap_or(Size::ZERO())
+                                    .evaluate(parent_bounds, Axis::Y);
+                                to_glass.get()
+                                    * parent_t_and_b.transform
+                                    * Point2::new(local_x, local_y)
+                            },
+                            &deps,
+                        )
+                    },
+                    layout_properties: n.layout_properties(),
                     id,
                 })
             })
@@ -178,29 +205,58 @@ impl ActionContext<'_> {
             .iter()
             .map(|i| i.transform_and_bounds.clone())
             .collect();
-        // TODO make conversion nicer (remove AxisAlignedBox? Or make conversion methods nice)
         let total_bounds = Property::computed(
             move || {
-                let axis_box = AxisAlignedBox::bound_of_points(bounds.iter().flat_map(|t_and_b| {
-                    let t_and_b = t_and_b.get();
-                    let (o, u, v) = t_and_b.transform.decompose();
-                    let u = u * t_and_b.bounds.0;
-                    let v = v * t_and_b.bounds.1;
-                    [o, o + v, o + u, o + v + u]
-                }));
-                let transform = Transform2::compose(
-                    axis_box.top_left(),
-                    Vector2::new(axis_box.width(), 0.0),
-                    Vector2::new(0.0, axis_box.height()),
-                );
-                TransformAndBounds {
-                    transform,
-                    bounds: (1.0, 1.0),
+                if bounds.len() == 1 {
+                    let t_and_b = bounds[0].get();
+                    let transform = t_and_b.transform.cast_spaces();
+                    TransformAndBounds {
+                        transform,
+                        bounds: t_and_b.bounds,
+                    }
+                } else {
+                    let axis_box =
+                        AxisAlignedBox::bound_of_points(bounds.iter().flat_map(|t_and_b| {
+                            let t_and_b = t_and_b.get();
+                            let (o, u, v) = t_and_b.transform.decompose();
+                            let u = u * t_and_b.bounds.0;
+                            let v = v * t_and_b.bounds.1;
+                            [o, o + v, o + u, o + v + u]
+                        }));
+                    let transform = Transform2::compose(
+                        axis_box.top_left(),
+                        Vector2::new(axis_box.width(), 0.0),
+                        Vector2::new(0.0, axis_box.height()),
+                    );
+                    TransformAndBounds {
+                        transform,
+                        bounds: (1.0, 1.0),
+                    }
                 }
             },
             &deps,
         );
+        let origin: Vec<_> = items.iter().map(|i| i.origin.clone()).collect();
+        let total_origin = if origin.len() == 1 {
+            origin[0].clone()
+        } else {
+            let deps = [total_bounds.untyped()];
+            let t_b = total_bounds.clone();
+            Property::computed(
+                move || {
+                    let t_b = t_b.get();
+                    let (o, u, v) = t_b.transform.decompose();
+                    let u = u * t_b.bounds.0;
+                    let v = v * t_b.bounds.1;
+                    let center = o + u / 2.0 + v / 2.0;
+                    center
+                },
+                &deps,
+            )
+        };
+
         SelectionState {
+            total_origin,
             items,
             total_bounds,
         }
