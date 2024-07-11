c4709c9e9d5b81e40ccbdff82d9a2449d4f5f04a
Samuel Selleck <samuel@pax.dev>
2024-07-11 15:14:25 -0700

    moved stacker control logic to general movement (duplicated for now)

--- pax-designer/pax-designer/src/model/tools.rs
+++ pax-designer/pax-designer/src/model/tools.rs
@@ -2,7 +2,7 @@ use std::any::Any;
 use std::ops::ControlFlow;
 use std::rc::Rc;
 
-use super::action::orm::{CreateComponent, SetNodePropertiesFromTransform};
+use super::action::orm::{CreateComponent, SetNodeLayout, SetNodePropertiesFromTransform};
 use super::action::pointer::Pointer;
 use super::action::{Action, ActionContext, RaycastMode};
 use super::input::InputEvent;
@@ -30,7 +30,7 @@ use pax_manifest::{
     PaxType, TemplateNodeId, TreeIndexPosition, TypeId, UniqueTemplateNodeIdentifier,
 };
 use pax_runtime_api::math::Transform2;
-use pax_runtime_api::{Axis, Window};
+use pax_runtime_api::{borrow, Axis, Window};
 use pax_std::stacker::Stacker;
 
 pub struct CreateComponentTool {
@@ -216,6 +216,34 @@ impl PointerTool {
                 }
                 .perform(ctx);
             }
+
+            // set visualization outline to always be the bounds of the parent of the moving node
+            let dt = borrow!(ctx.engine_context.designtime);
+            let manifest_ver = dt.get_orm().get_manifest_version();
+            let glass_transform = ctx.glass_transform();
+            let slot_child_index = hit.global_id().unwrap().clone();
+            let deps = [glass_transform.untyped(), manifest_ver.untyped()];
+            let ctx_e = ctx.engine_context.clone();
+            let vis = Property::computed(
+                move || {
+                    let slot_child_parent = ctx_e
+                        .get_nodes_by_global_id(slot_child_index.clone())
+                        .into_iter()
+                        .next()
+                        .unwrap()
+                        .render_parent()
+                        .unwrap();
+                    let slot_child_parent = GlassNode::new(&slot_child_parent, &glass_transform);
+                    let outline =
+                        PathOutline::from_bounds(slot_child_parent.transform_and_bounds.get());
+                    ToolVisualizationState {
+                        rect_tool: Default::default(),
+                        outline,
+                    }
+                },
+                &deps,
+            );
+
             let selection = ctx.derived_state.selection_state.get();
             Self {
                 action: PointerToolAction::Moving {
@@ -223,7 +251,7 @@ impl PointerTool {
                     has_moved: false,
                     pickup_point: point,
                     initial_selection: (&selection).into(),
-                    vis: Property::new(ToolVisualizationState::default()),
+                    vis,
                 },
             }
         } else {
@@ -280,10 +308,19 @@ impl ToolBehaviour for PointerTool {
                 };
 
                 for item in &initial_selection.items {
+                    let curr_node = ctx
+                        .engine_context
+                        .get_nodes_by_global_id(item.id.clone())
+                        .into_iter()
+                        .next()
+                        .unwrap();
+                    let glass_curr_node = GlassNode::new(&curr_node, &ctx.glass_transform());
                     if let Err(e) = (SetNodePropertiesFromTransform {
                         id: &item.id,
                         transform_and_bounds: &(move_translation * item.transform_and_bounds),
-                        parent_transform_and_bounds: &item.parent_transform_and_bounds,
+                        parent_transform_and_bounds: &glass_curr_node
+                            .parent_transform_and_bounds
+                            .get(),
                         decomposition_config: &item.layout_properties.into_decomposition_config(),
                     }
                     .perform(ctx))
@@ -291,20 +328,102 @@ impl ToolBehaviour for PointerTool {
                         pax_engine::log::error!("Error moving selected: {:?}", e);
                     }
                 }
+                if initial_selection.items.len() != 1 {
+                    return ControlFlow::Continue(());
+                }
 
-                let raycast_hit = raycast_slot(ctx, point, hit.clone(), true);
-                if let Some((_container, slot)) = raycast_hit {
-                    let t_and_b = TransformAndBounds {
-                        transform: ctx.glass_transform().get(),
-                        bounds: (1.0, 1.0),
-                    } * slot.transform_and_bounds().get();
-                    let outline = PathOutline::from_bounds(t_and_b);
-                    vis.set(ToolVisualizationState {
-                        rect_tool: Default::default(),
-                        outline,
-                    });
-                } else {
-                    vis.set(ToolVisualizationState::default());
+                let item = initial_selection.items[0].clone();
+
+                let curr_node = ctx
+                    .engine_context
+                    .get_nodes_by_global_id(item.id.clone())
+                    .into_iter()
+                    .next()
+                    .unwrap();
+                let curr_container = curr_node.template_parent().unwrap();
+                let glass_curr_container = GlassNode::new(&curr_container, &ctx.glass_transform());
+
+                let move_translation = TransformAndBounds {
+                    transform: Transform2::translate(translation),
+                    bounds: (1.0, 1.0),
+                };
+
+                if let Some((container, slot_hit)) =
+                    raycast_slot(ctx, point, curr_node.clone(), false)
+                {
+                    let curr_slot = curr_node.render_parent().unwrap();
+
+                    let new_index =
+                        slot_hit.with_properties(|f: &mut Slot| f.index.get().to_int() as usize);
+                    let old_index =
+                        curr_slot.with_properties(|f: &mut Slot| f.index.get().to_int() as usize);
+
+                    if curr_slot == slot_hit && new_index == old_index {
+                        return ControlFlow::Continue(());
+                    }
+
+                    let glass_slot_hit = GlassNode::new(&slot_hit, &ctx.glass_transform());
+                    if let Err(e) = (MoveNode {
+                        node_id: &item.id,
+                        new_parent_uid: &container.global_id().unwrap(),
+                        index: pax_manifest::TreeIndexPosition::At(new_index.unwrap()),
+                        // TODO try to make this the "future calculated position
+                        // after ORM updates" instead. might be possible to
+                        // subscribe to manifest changes, and update the bounds
+                        // whenever that happens, but still takes 2 ticks (manifest
+                        // update -> recalc bounds -> manifest update). Leave as
+                        // this for now as the problem is only visible from first
+                        // movement over a new slot to the next mouse-move op WHEN
+                        // the slot bounds will change as a consequence of movement.
+                        node_layout: NodeLayoutSettings::KeepScreenBounds {
+                            node_transform_and_bounds: &(move_translation
+                                * item.transform_and_bounds),
+                            new_parent_transform_and_bounds: &glass_slot_hit
+                                .transform_and_bounds
+                                .get(),
+                            node_decompositon_config: item
+                                .layout_properties
+                                .into_decomposition_config(),
+                        },
+                    }
+                    .perform(ctx))
+                    {
+                        log::warn!("failed to swap nodes: {}", e);
+                    };
+                } else if !glass_curr_container
+                    .transform_and_bounds
+                    .get()
+                    .contains_point(point)
+                    && ctx
+                        .engine_context
+                        .get_nodes_by_id(ROOT_PROJECT_ID)
+                        .into_iter()
+                        .next()
+                        .unwrap()
+                        != curr_container
+                {
+                    let container_parent = curr_container.template_parent().unwrap();
+                    let container_parent =
+                        GlassNode::new(&container_parent, &ctx.glass_transform());
+                    if let Err(e) = (MoveNode {
+                        node_id: &item.id,
+                        new_parent_uid: &container_parent.id,
+                        index: pax_manifest::TreeIndexPosition::Top,
+                        node_layout: NodeLayoutSettings::KeepScreenBounds {
+                            node_transform_and_bounds: &(move_translation
+                                * item.transform_and_bounds),
+                            new_parent_transform_and_bounds: &container_parent
+                                .transform_and_bounds
+                                .get(),
+                            node_decompositon_config: item
+                                .layout_properties
+                                .into_decomposition_config(),
+                        },
+                    }
+                    .perform(ctx))
+                    {
+                        log::warn!("failed to swap nodes: {}", e);
+                    };
                 }
             }
             PointerToolAction::Selecting { ref mut p2, .. } => *p2 = point,
@@ -343,30 +462,24 @@ impl ToolBehaviour for PointerTool {
             PointerToolAction::Moving {
                 has_moved, ref hit, ..
             } => {
-                // TODO add check here that we're not moving something from within the same stacker
                 if *has_moved {
-                    if let Some((container, slot)) = raycast_slot(ctx, point, hit.clone(), true) {
-                        if let Err(e) = (MoveNode::<Glass> {
-                            node_id: &hit.global_id().unwrap(),
-                            new_parent_uid: &container.global_id().unwrap(),
-                            index: pax_manifest::TreeIndexPosition::At(
-                                slot.with_properties(|f: &mut Slot| {
-                                    f.index.get().to_int() as usize
-                                })
-                                .unwrap(),
-                            ),
-                            node_layout: NodeLayoutSettings::Fill,
+                    let hit_id = hit.global_id().unwrap();
+                    let curr_node = ctx
+                        .engine_context
+                        .get_nodes_by_global_id(hit_id.clone())
+                        .into_iter()
+                        .next()
+                        .unwrap();
+                    if curr_node.render_parent().unwrap().is_of_type::<Slot>() {
+                        if let Err(e) = (SetNodeLayout {
+                            id: &hit_id,
+                            node_layout: &NodeLayoutSettings::Fill::<Glass>,
                         }
                         .perform(ctx))
                         {
-                            log::warn!("failed to swap nodes: {}", e);
-                        };
-                        let _ = SelectNodes {
-                            ids: &[container.global_id().unwrap().get_template_node_id()],
-                            overwrite: true,
+                            log::warn!("failed: {e}")
                         }
-                        .perform(ctx);
-                    };
+                    }
                 } else {
                     let _ = SelectNodes {
                         ids: &[hit.global_id().unwrap().get_template_node_id()],
