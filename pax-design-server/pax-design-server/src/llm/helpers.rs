668427c66d52871d76421d059c78ddb2bf95274d
Zack Brown <zack@pax.dev>
2024-07-24 16:16:09 -0700

    wip: refactoring

--- pax-design-server/pax-design-server/src/llm/helpers.rs
+++ pax-design-server/pax-design-server/src/llm/helpers.rs
@@ -14,8 +14,6 @@ use super::simple::{
     SimplePixel, SimpleProperties, SimpleRotation, SimpleTemplate,
 };
 
-use super::constants::PREFIX;
-
 pub fn convert_to_simple_node_info(
     id: &TemplateNodeId,
     node: &TemplateNodeDefinition,
@@ -56,10 +54,10 @@ impl From<ComponentTemplate> for SimpleTemplate {
 pub fn simple_node_type_to_type_id(node_type: SimpleNodeType) -> Option<TypeId> {
     let t = match node_type {
         SimpleNodeType::Rectangle => {
-            TypeId::build_singleton(&format!("{}::Rectangle", PREFIX), None)
+            TypeId::build_singleton("pax_std::drawing::rectangle::Rectangle", None)
         }
-        SimpleNodeType::Ellipse => TypeId::build_singleton(&format!("{}::Ellipse", PREFIX), None),
-        SimpleNodeType::Text => TypeId::build_singleton(&format!("{}::Text", PREFIX), None),
+        SimpleNodeType::Ellipse => TypeId::build_singleton("pax_std::drawing::ellipse::Ellipse", None),
+        SimpleNodeType::Text => TypeId::build_singleton("pax_std::core::text::Text", None),
         SimpleNodeType::Navbar => TypeId::build_singleton(
             "designer_project::menu_bar::MenuBar",
             None,
