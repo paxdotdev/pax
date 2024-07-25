7bd2e2b5a0ea57b5aee9fcac0413dc7ee228d799
Zack Brown <zack@pax.dev>
2024-07-24 17:34:03 -0700

    Revert "wip"
    This reverts commit bea1f7f9a922d94cf5645c7ec977205f417aa0f7.

--- pax-design-server/pax-design-server/tests/data/code_serialization/serialization_test_project/src/generated_lib.rs
+++ pax-design-server/pax-design-server/tests/data/code_serialization/serialization_test_project/src/generated_lib.rs
@@ -2,7 +2,11 @@
 
 use pax_engine::*;
 use pax_engine::api::*;
-use pax_std::*;
+use pax_std::primitives::*;
+use pax_std::types::*;
+use pax_std::types::text::*;
+use pax_std::components::*;
+use pax_std::components::Stacker;
 
 #[pax]
 #[main]
