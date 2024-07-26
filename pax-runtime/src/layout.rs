10f4c9bc43daa328480f8f0100971316b3405153
Zack Brown <zack@pax.dev>
2024-07-26 15:10:31 -0700

    cleaning

--- pax-runtime/src/layout.rs
+++ pax-runtime/src/layout.rs
@@ -1,6 +1,6 @@
 use std::ops::Mul;
 
-use pax_runtime_api::math::{Parts, Point2, Space};
+use pax_runtime_api::math::{Generic, Parts, Point2, Space};
 use pax_runtime_api::{Interpolatable, Percent, Property, Rotation, Window};
 
 use crate::api::math::{Transform2, Vector2};
