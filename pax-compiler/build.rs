7629ab8c74b08b73cbf745a62840016dbd4dd70c
Zack Brown <zack@pax.dev>
2024-08-13 17:21:03 +0700

    wip: toward workspace build

---
+++ pax-compiler/build.rs
@@ -0,0 +1,12 @@
+use std::env;
+use std::fs;
+use std::io::{self, Write};
+use std::path::Path;
+use std::process::Command;
+
+fn main() -> io::Result<()> {
+    //Problem: circular reference (path unclear)
+
+
+    Ok(())
+}
