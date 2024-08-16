2065c006190580ee8910fd4083307a91689a8d29
Zack Brown <zack@pax.dev>
2024-08-16 14:05:40 +0700

    re-add static serving logic

---
+++ pax-compiler/src/design_server/static_server.rs
@@ -0,0 +1,49 @@
+use std::net::TcpListener;
+use std::path::PathBuf;
+use actix_web::{App, HttpServer};
+use actix_web::middleware::Logger;
+use colored::Colorize;
+use env_logger;
+use std::io::Write;
+use crate::helpers::PAX_BADGE;
+
+pub fn start_server(fs_path: PathBuf) -> std::io::Result<()> {
+    // Initialize logging
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::Builder::from_env(env_logger::Env::default())
+        .format(|buf, record| writeln!(buf, "{} 🍱 Served {}", *PAX_BADGE, record.args()))
+        .init();
+
+    // Create a Runtime
+    let runtime = actix_web::rt::System::new().block_on(async {
+        let mut port = 8080;
+        let server = loop {
+            // Check if the port is available
+            if TcpListener::bind(("127.0.0.1", port)).is_ok() {
+                // Log the server details
+                println!(
+                    "{} 🗂️  Serving static files from {}",
+                    *PAX_BADGE,
+                    &fs_path.to_str().unwrap()
+                );
+                let address_msg = format!("http://127.0.0.1:{}", port).blue();
+                let server_running_at_msg = format!("Server running at {}", address_msg).bold();
+                println!("{} 📠 {}", *PAX_BADGE, server_running_at_msg);
+                break HttpServer::new(move || {
+                    App::new().wrap(Logger::new("| %s | %U")).service(
+                        actix_files::Files::new("/*", fs_path.clone()).index_file("index.html"),
+                    )
+                })
+                    .bind(("127.0.0.1", port))
+                    .expect("Error binding to address")
+                    .workers(2);
+            } else {
+                port += 1; // Try the next port
+            }
+        };
+
+        server.run().await
+    });
+
+    runtime
+}
