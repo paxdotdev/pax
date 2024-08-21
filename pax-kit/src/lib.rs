12813d7e0449bbc2f5bc9dea78b64ab31fa4da7c
Zack Brown <zack@pax.dev>
2024-08-21 12:37:46 +0700

    wip: pax-kit

--- pax-kit/src/lib.rs
+++ pax-kit/src/lib.rs
@@ -1,5 +1,9 @@
+//Reexport primary modules
 pub use pax_designer;
+pub use pax_engine;
 pub use pax_std;
+
+//Splat-export certain curated modules, for ergo
 pub use pax_engine::*;
 pub use pax_engine::api::*;
 pub use pax_std::*;
