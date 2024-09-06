d2dca8a30603723447c094b431eb106d7fd2f265
Samuel Selleck <samuel@pax.dev>
2024-09-06 16:46:10 -0700

    designtime integration test fix

--- pax-compiler/tests/designtime_integration_test.rs
+++ pax-compiler/tests/designtime_integration_test.rs
@@ -1,11 +1,15 @@
-// use std::collections::{BTreeMap, HashMap};
+use std::{
+    collections::{BTreeMap, HashMap},
+    env,
+    time::Duration,
+};
 
 use actix_web::{web::Data, App};
 use pax_compiler::design_server::{web_socket, AppState};
-// use pax_manifest::{
-//     ComponentDefinition, ComponentTemplate, LiteralBlockDefinition, PaxManifest,
-//     SettingsBlockElement, TemplateNodeDefinition, Token, TypeId,
-// };
+use pax_manifest::{
+    ComponentDefinition, ComponentTemplate, LiteralBlockDefinition, PaxManifest,
+    SettingsBlockElement, TemplateNodeDefinition, Token, TypeId,
+};
 
 pub fn get_test_server() -> actix_test::TestServer {
     actix_test::start(|| {
@@ -15,90 +19,87 @@ pub fn get_test_server() -> actix_test::TestServer {
     })
 }
 
-// fn create_basic_manifest(source_path: String) -> PaxManifest {
-//     let mut components = BTreeMap::new();
-//     let component_type_id = TypeId::build_singleton("Component1", Some("Component1"));
-//     let special_component_type_id =
-//         TypeId::build_singleton("SpecialComponent", Some("SpecialComponent"));
-//     let mut template = ComponentTemplate::new(component_type_id.clone(), Some(source_path));
+fn create_basic_manifest(source_path: String) -> PaxManifest {
+    let mut components = BTreeMap::new();
+    let component_type_id = TypeId::build_singleton("Component1", Some("Component1"));
+    let special_component_type_id =
+        TypeId::build_singleton("SpecialComponent", Some("SpecialComponent"));
+    let mut template = ComponentTemplate::new(component_type_id.clone(), Some(source_path));
 
-//     template.add(TemplateNodeDefinition {
-//         type_id: special_component_type_id,
-//         control_flow_settings: None,
-//         settings: None,
-//         raw_comment_string: None,
-//     });
+    template.add(TemplateNodeDefinition {
+        type_id: special_component_type_id,
+        control_flow_settings: None,
+        settings: None,
+        raw_comment_string: None,
+    });
 
-//     // .add puts the node on the top of the template by default in line with designing
-//     template.add(TemplateNodeDefinition {
-//         type_id: TypeId::build_comment(),
-//         control_flow_settings: None,
-//         settings: None,
-//         raw_comment_string: Some("// Hello world\n".to_owned()),
-//     });
+    // .add puts the node on the top of the template by default in line with designing
+    template.add(TemplateNodeDefinition {
+        type_id: TypeId::build_comment(),
+        control_flow_settings: None,
+        settings: None,
+        raw_comment_string: Some("// Hello world\n".to_owned()),
+    });
 
-//     components.insert(
-//         component_type_id.clone(),
-//         ComponentDefinition {
-//             type_id: component_type_id.clone(),
-//             is_main_component: false,
-//             is_primitive: false,
-//             is_struct_only_component: false,
-//             module_path: "module_path1".to_string(),
-//             primitive_instance_import_path: None,
-//             template: Some(template),
-//             settings: Some(vec![
-//                 SettingsBlockElement::SelectorBlock(
-//                     Token::new_without_location("#existing_selector".to_string()),
-//                     LiteralBlockDefinition::new(vec![]),
-//                 ),
-//                 SettingsBlockElement::Handler(
-//                     Token::new_without_location("@existing_handler".to_string()),
-//                     vec![Token::new_without_location("handler_action".to_string())],
-//                 ),
-//             ]),
-//         },
-//     );
+    components.insert(
+        component_type_id.clone(),
+        ComponentDefinition {
+            type_id: component_type_id.clone(),
+            is_main_component: false,
+            is_primitive: false,
+            is_struct_only_component: false,
+            module_path: "module_path1".to_string(),
+            primitive_instance_import_path: None,
+            template: Some(template),
+            settings: Some(vec![
+                SettingsBlockElement::SelectorBlock(
+                    Token::new_without_location("#existing_selector".to_string()),
+                    LiteralBlockDefinition::new(vec![]),
+                ),
+                SettingsBlockElement::Handler(
+                    Token::new_without_location("existing_handler".to_string()),
+                    vec![Token::new_without_location("handler_action".to_string())],
+                ),
+            ]),
+        },
+    );
 
-//     PaxManifest {
-//         components,
-//         main_component_type_id: component_type_id,
-//         type_table: HashMap::new(),
-//         assets_dirs: vec![],
-//         engine_import_path: "".to_string(),
-//     }
-// }
+    PaxManifest {
+        components,
+        main_component_type_id: component_type_id,
+        type_table: HashMap::new(),
+        assets_dirs: vec![],
+        engine_import_path: "".to_string(),
+    }
+}
 
 #[actix_web::test]
 async fn designtime_integration_test() {
-    todo!()
-    // TODO add back once we figure out privileged agent connection structure
+    const EXPECTED_PAX: &str = "// Hello world
+<SpecialComponent />
 
-    // const EXPECTED_PAX: &str = "// Hello world
-    // <SpecialComponent />
+@settings {
+    @existing_handler: handler_action
+    #existing_selector {
+\x20\x20\x20\x20
+    }
+}";
 
-    // @settings {
-    //     @existing_handler: handler_action,
-    //     #existing_selector {
+    let component_type_id = TypeId::build_singleton("Component1", Some("Component1"));
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+    let path = current_dir.join("tests/data/designtime_integration_test.pax");
+    let path_str = path.to_str().expect("Path is not a valid UTF-8 string");
 
-    //     }
-    // }";
+    let srv = get_test_server();
 
-    // let component_type_id = TypeId::build_singleton("Component1", Some("Component1"));
-    // let current_dir = env::current_dir().expect("Failed to get current directory");
-    // let path = current_dir.join("tests/data/designtime_integration_test.pax");
-    // let path_str = path.to_str().expect("Path is not a valid UTF-8 string");
+    let manifest: PaxManifest = create_basic_manifest(path_str.to_owned());
+    let mut designer = pax_designtime::DesigntimeManager::new_with_addr(manifest, srv.addr());
+    designer.send_component_update(&component_type_id).unwrap();
 
-    // let srv = get_test_server();
+    std::thread::sleep(Duration::from_secs(1));
 
-    // let manifest: PaxManifest = create_basic_manifest(path_str.to_owned());
-    // let mut designer = pax_designtime::DesigntimeManager::new_with_addr(manifest, srv.addr());
-    // designer.send_component_update(&component_type_id).unwrap();
-
-    // std::thread::sleep(Duration::from_secs(1));
-
-    // let output = std::fs::read_to_string(path_str).expect("Failed to read output file");
-    // assert_eq!(output, EXPECTED_PAX);
-    // std::fs::write(path_str, b"FILE HAS NOT BEEN UPDATED BY DESIGNTIME")
-    //     .expect("couldn't reset file");
+    let output = std::fs::read_to_string(path_str).expect("Failed to read output file");
+    assert_eq!(output, EXPECTED_PAX);
+    std::fs::write(path_str, b"FILE HAS NOT BEEN UPDATED BY DESIGNTIME")
+        .expect("couldn't reset file");
 }
