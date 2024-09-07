b0a90a332998547f5bc6bf12738706bf5d372055
Warfa Jibril <warfaj@gmail.com>
2024-09-06 17:33:36 -0700

    carousel

---
+++ examples/src/carousel/src/lib.rs
@@ -0,0 +1,96 @@
+#![allow(unused_imports)]
+
+use pax_kit::*;
+
+#[pax]
+#[main]
+#[file("lib.pax")]
+pub struct Example {}
+
+#[pax]
+#[file("carousel.pax")]
+pub struct Carousel {
+    pub ticks: Property<usize>,
+    pub cell_specs: Property<Vec<CarouselCell>>,
+    pub current_cell: Property<usize>,
+    pub current_cell_on_change: Property<bool>,
+    pub transition: Property<f64>,
+}
+
+#[pax]
+pub struct CarouselCell {
+    pub is_active: bool,
+    pub x_percent: f64,
+}
+
+impl Carousel {
+    pub fn on_mount(&mut self, ctx: &NodeContext) {
+        let slot_children_count = ctx.slot_children_count.clone();
+        let current_cell = self.current_cell.clone();
+        let transition = self.transition.clone();
+
+        let deps = [
+            slot_children_count.untyped(),
+            current_cell.untyped(),
+            transition.untyped(),
+        ];
+
+        self.cell_specs.replace_with(Property::computed(
+            move || {
+                let slot_children_count = slot_children_count.get();
+                let current_cell = current_cell.get();
+                let transition = transition.get();
+
+                let mut cell_specs = vec![];
+                for i in 0..slot_children_count {
+                    let is_active = match i as isize - current_cell as isize {
+                        _ => true,
+                    };
+
+                    let x_percent = 50.0 + ((i as f64 *100.0) - transition);
+
+                    cell_specs.push(CarouselCell {
+                        is_active,
+                        x_percent: x_percent,
+                    });
+                }
+
+                cell_specs
+            },
+            &deps,
+        ));
+
+        let current_cell = self.current_cell.clone();
+        let transition = self.transition.clone();
+        let deps = [current_cell.untyped()];
+
+        self.current_cell_on_change.replace_with(Property::computed(
+            move || {
+                let current_cell = current_cell.get();
+                transition.ease_to(current_cell as f64 *100.0, 60, EasingCurve::OutQuad);
+                false
+            },
+            &deps,
+        ));
+    }
+
+    pub fn update(&mut self, _ctx: &NodeContext) {
+        self.current_cell_on_change.get();
+    }
+
+    pub fn increment(&mut self, ctx: &NodeContext, _args: Event<Click>) {
+        let current_cell = self.current_cell.get();
+        let children_count =  ctx.slot_children_count.get();
+        self.current_cell.set((current_cell + 1) % children_count);
+    }
+
+    pub fn decrement(&mut self, ctx: &NodeContext, _args: Event<Click>) {
+        let current_cell = self.current_cell.get();
+        let children_count =  ctx.slot_children_count.get();
+        if current_cell != 0 {
+            self.current_cell.set((current_cell - 1) % children_count);
+        } else {
+            self.current_cell.set(children_count - 1);
+        }
+    }
+}
