4b5779cd51fbf951ed993e1b9ab754cb88cf89ad
Warfa Jibril <warfaj@gmail.com>
2024-09-09 13:01:57 -0700

    updated starter project template

--- pax-compiler/files/new-project/new-project-template/src/lib.rs
+++ pax-compiler/files/new-project/new-project-template/src/lib.rs
@@ -3,7 +3,19 @@
 use pax_kit::*;
 
 pub mod calculator;
-pub use calculator::Calculator;
+pub use calculator::*;
+
+pub mod fireworks;
+pub use fireworks::*;
+
+pub mod space_game;
+pub use space_game::*;
+
+pub mod color_picker;
+pub use color_picker::*;
+
+mod animation;
+
 
 #[pax]
 #[main]
