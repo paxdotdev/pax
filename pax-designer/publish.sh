31d39b3db5ec618371212fb834990a34d22b8c38
Zack Brown <zack@pax.dev>
2024-09-10 09:02:48 -0700

    cleaning

--- pax-designer/publish.sh
+++
@@ -1,3 +0,0 @@
-rm -r pax-create-client/create
-mv pax-designer/.pax/build/release/web pax-create-client/create
-mv pax-designer/.pax/pkg/pax-cartridge/initial-manifest.json pax-create-server/templates/manifest_new_project_template.json
