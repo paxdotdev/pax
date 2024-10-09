2c2a3ade973d094b1beb781d77211830a3750373
Zack Brown <zack@pax.dev>
2024-10-09 12:39:21 -0700

    remove all attributes from DOM nodes when recycling

--- pax-compiler/files/interfaces/web/src/pools/object-pool.ts
+++ pax-compiler/files/interfaces/web/src/pools/object-pool.ts
@@ -1,4 +1,4 @@
-export class ObjectPool<T> {
+export class ObjectPool<T extends HTMLElement> {
     private pool: T[] = [];
     private readonly factory: (args?: any) => T;
     private readonly cleanUp: (item: T) => void;
@@ -17,6 +17,11 @@ export class ObjectPool<T> {
 
     put(item: T) {
         this.cleanUp(item);
+        if ((item as HTMLElement).attributes !== undefined)
+        {
+            const attrs = Array.from(item.attributes);
+            attrs.forEach(attr => item.removeAttribute(attr.name));
+        }
         this.pool.push(item);
     }
 }
