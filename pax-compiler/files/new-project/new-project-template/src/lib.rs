8b5bf27fa9597ba3450e66940118b0fd5ad172a6
Zack Brown <zack@pax.dev>
2024-10-09 17:03:53 -0700

    update increment example + ease_to logic

--- pax-compiler/files/new-project/new-project-template/src/lib.rs
+++ pax-compiler/files/new-project/new-project-template/src/lib.rs
@@ -1,22 +1,4 @@
-#![allow(unused_imports)]
-
 use pax_kit::*;
-
-pub mod calculator;
-pub use calculator::*;
-
-pub mod fireworks;
-pub use fireworks::*;
-
-pub mod space_game;
-pub use space_game::*;
-
-pub mod color_picker;
-pub use color_picker::*;
-
-mod animation;
-
-
 #[pax]
 #[main]
 #[file("lib.pax")]
