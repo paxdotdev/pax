69dbc49e3bb9b0caad585077e4cddcfae537351a
Zack Brown <zack@pax.dev>
2024-10-09 17:05:33 -0700

    update starter project template

--- pax-compiler/files/new-project/new-project-template/src/lib.rs
+++ pax-compiler/files/new-project/new-project-template/src/lib.rs
@@ -1,11 +1,16 @@
 use pax_kit::*;
+
 #[pax]
 #[main]
 #[file("lib.pax")]
 pub struct Example {
     pub ticks: Property<usize>,
     pub num_clicks: Property<usize>,
+    pub current_rotation: Property<f64>,
 }
+
+const ROTATION_INCREMENT_DEGREES: f64 = 90.0;
+const ROTATION_EASING_DURATION_FRAMES: u64 = 120;
 
 impl Example {
     pub fn handle_pre_render(&mut self, _ctx: &NodeContext) {
@@ -15,6 +20,8 @@ impl Example {
 
     pub fn increment(&mut self, _ctx: &NodeContext, _args: Event<Click>) {
         let old_num_clicks = self.num_clicks.get();
-        self.num_clicks.set(old_num_clicks + 1);
+        let new_val = old_num_clicks + 1;
+        self.num_clicks.set(new_val);
+        self.current_rotation.ease_to(new_val as f64 * ROTATION_INCREMENT_DEGREES, ROTATION_EASING_DURATION_FRAMES, EasingCurve::OutQuad);
     }
 }
