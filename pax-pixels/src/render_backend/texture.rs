952e4bb6ff665f0b455f4a01a2b532d14b7045f7
Zack Brown <zack@pax.dev>
2024-10-31 14:26:29 -0700

    revert partial MSAA work

--- pax-pixels/src/render_backend/texture.rs
+++ pax-pixels/src/render_backend/texture.rs
@@ -197,7 +197,7 @@ impl TextureRenderer {
             label: Some("Texture Creation"),
             size,
             mip_level_count: 1,
-            sample_count: 4,
+            sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
             format: wgpu::TextureFormat::Rgba8UnormSrgb,
             usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
